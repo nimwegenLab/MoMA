8b7ec45682d3527f90079a42494543a7dec46e28
Michael Mell <michael.mell@gmx.net>
2019-09-18 12:07:50 +0200

    Ran code-cleanup

--- src/main/java/com/jug/util/componenttree/SimpleComponent.java
+++ src/main/java/com/jug/util/componenttree/SimpleComponent.java
@@ -5,36 +5,38 @@ import net.imglib2.Localizable;
 import net.imglib2.algorithm.componenttree.Component;
 import net.imglib2.algorithm.componenttree.pixellist.PixelList;
 import net.imglib2.type.Type;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-public final class SimpleComponent <T extends Type<T>>
+public final class SimpleComponent<T extends Type<T>>
         implements
         Component<T, SimpleComponent<T>> {
-
-    /**
-     * child nodes in the {@link FilteredComponentTree}.
-     */
-    private ArrayList<SimpleComponent< T >> children  = new ArrayList<>();
-
-    /**
-     * parent node in the {@link FilteredComponentTree}.
-     */
-    private SimpleComponent< T > parent;
 
     /**
      * Pixels in the component.
      */
     private final PixelList pixelList;
-
     /**
      * Maximum threshold value of the connected component.
      */
     private final T value;
+    /**
+     * child nodes in the {@link FilteredComponentTree}.
+     */
+    private ArrayList<SimpleComponent<T>> children = new ArrayList<>();
+    /**
+     * parent node in the {@link FilteredComponentTree}.
+     */
+    private SimpleComponent<T> parent;
+    /**
+     * We need to reverse the list of children, so that the division assignment has the children in the correct order
+     * (meaning top and bottom children are assign correctly). This is a hackish work around to solve an issue that I
+     * do not fully understand yet and should probably be fixed here: ComponentTreeUtils.getRightNeighbor(...)
+     */
+    private Boolean childrenWereReversed = false;
 
     /**
      * Constructor for fully connected component-node (with parent or children).
@@ -63,19 +65,16 @@ public final class SimpleComponent <T extends Type<T>>
         this.parent = parent;
     }
 
-    private Boolean childrenWereReversed = false;
-
     @Override
     public List<SimpleComponent<T>> getChildren() {
-        if(!childrenWereReversed){
+        if (!childrenWereReversed) {
             Collections.reverse(children);
             childrenWereReversed = true;
         }
-
         return children;
     }
 
-    void addChild(SimpleComponent< T > child){
+    void addChild(SimpleComponent<T> child) {
         this.children.add(child);
     }
 

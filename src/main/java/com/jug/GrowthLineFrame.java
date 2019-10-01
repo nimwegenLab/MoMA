456c4d110289fe48fa975f109a6f312ff7017a17
Michael Mell <michael.mell@gmx.net>
2019-10-01 15:40:23 +0200

    Comment out the showing of segments at program start and profiling. Was used for debugging.

--- src/main/java/com/jug/GrowthLineFrame.java
+++ src/main/java/com/jug/GrowthLineFrame.java
@@ -63,7 +63,7 @@ public class GrowthLineFrame extends AbstractGrowthLineFrame< FilteredComponent<
 		}
 
 //		MserTree<FloatType> componentTree = MserTree.buildMserTree(raiFkt, 0.01, 0, Long.MAX_VALUE, 0.0005, 0.4, false);
-		long startTime = System.nanoTime();
+//		long startTime = System.nanoTime();
         final double delta = 0.001;
         final int minSize = 50; // minSize=50px seems safe, assuming pixel-area of a round cell with radius of have the bacterial width: 3.141*0.35**2/0.065**2, where pixelSize=0.065mu and width/2=0.35mu
         final long maxValue = Long.MAX_VALUE;
@@ -71,10 +71,10 @@ public class GrowthLineFrame extends AbstractGrowthLineFrame< FilteredComponent<
         final double minDiversity = 0.4;
         final boolean darkToBright = false;
         MserTree<FloatType> componentTree = MserTree.buildMserTree(raiFkt, delta, minSize, maxValue, maxVar, minDiversity, darkToBright);
-		long stopTime = System.nanoTime();
-		System.out.println("Elapsed time:" + (stopTime-startTime)/1000000);
+//		long stopTime = System.nanoTime();
+//		System.out.println("Elapsed time:" + (stopTime-startTime)/1000000);
 
-        Plotting.drawComponentTree2(componentTree, new ArrayList<>());
+//        Plotting.drawComponentTree2(componentTree, new ArrayList<>());
 
 		Predicate<Integer> widthCondition = (width) -> (width <= 20);
 		ILocationTester ctester = new ComponentExtentTester(0, widthCondition);

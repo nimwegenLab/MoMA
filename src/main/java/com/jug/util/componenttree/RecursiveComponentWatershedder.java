8319b8f65f4c1b4361882498e0617141aa0214fe
Michael Mell <michael.mell@gmx.net>
2019-11-07 12:00:06 +0100

    Cleanup and add comments.

--- src/main/java/com/jug/util/componenttree/RecursiveComponentWatershedder.java
+++ src/main/java/com/jug/util/componenttree/RecursiveComponentWatershedder.java
@@ -26,7 +26,6 @@ import org.scijava.Context;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -34,49 +33,44 @@ import java.util.function.Consumer;
 public class RecursiveComponentWatershedder<T extends Type<T>, C extends Component<T, C>> {
     private OpService ops = (new Context()).service(OpService.class);
 
-    @NotNull
-    private RandomAccessibleInterval<BitType> getParentsMask(List<SimpleComponent<T>> parentsWithChildren) {
-        Img<T> sourceImage = ImgView.wrap(parentsWithChildren.get(0).getSourceImage(), new ArrayImgFactory(new FloatType()));
-        ImgLabeling<Integer, IntType> parentLabeling = createLabelingImage(sourceImage);
-        Integer label = 1;
-        for(SimpleComponent<T> parent : parentsWithChildren){ // write parents to mask
-            parent.writeLabels(parentLabeling, label);
-        }
-        RandomAccessibleInterval<BitType> mask = ops.convert().bit(Views.iterable(parentLabeling.getIndexImg()));
-        return mask;
-    }
-
-    @NotNull
-    private ImgLabeling<Integer, IntType> getChildLabeling(List<SimpleComponent<T>> parents, HashMap<Integer, SimpleComponent<T>> childLabelToComponentMap) {
-        Img<T> sourceImage = ImgView.wrap(parents.get(0).getSourceImage(), new ArrayImgFactory(new FloatType()));
-        ImgLabeling<Integer, IntType> childLabeling = createLabelingImage(sourceImage);
-        Integer childLabel = 1;
-        for(SimpleComponent<T> parent : parents){
-            List<SimpleComponent<T>> children = parent.getChildren();
-            for(SimpleComponent<T> child: children){
-                child.writeLabels(childLabeling, childLabel);
-                childLabelToComponentMap.put(childLabel, child);
-                ++childLabel;
+    /**
+     * Recursively grow child components into their corresponding parent components. After running this the child
+     * components in the tree will always possess the combined area as their corresponding parent component.
+     *
+     * @param tree
+     * @return
+     */
+    public SimpleComponentTree<T, C> recursivelyWatershedComponents(SimpleComponentTree<T, C> tree) {
+        Consumer<Pair<List<SimpleComponent<T>>, Integer>> levelComponentsConsumer = (levelComponentsListAndLevel) -> {
+            List<SimpleComponent<T>> componentsOfLevel = levelComponentsListAndLevel.getValue0();
+            {
+                watershedChildrenOfThisLevel(componentsOfLevel);
             }
-        }
-        return childLabeling;
+        };
+        ComponentTreeUtils.doForEachComponentInTreeLevel(tree, levelComponentsConsumer);
+        return tree;
     }
 
-    private void watershedChildrenIntoThisLevel(List<SimpleComponent<T>> componentsOfLevel) {
+    /**
+     * Perform the watershedding of all child components of the parents in {@param parentComponents}.
+     *
+     * @param parentComponents list of parent components
+     */
+    private void watershedChildrenOfThisLevel(List<SimpleComponent<T>> parentComponents) {
         List<SimpleComponent<T>> parentsWithChildren = new ArrayList<>();
-        for (SimpleComponent<T> parent : componentsOfLevel) {
+        for (SimpleComponent<T> parent : parentComponents) {
             if (parent.getChildren().size() != 0) {
                 parentsWithChildren.add(parent);
             }
         }
-        if (parentsWithChildren.size() == 0) return; // If no parent has children, we are done.
+        if (parentsWithChildren.size() == 0) return; /* If no parent has children, we are done. */
 
         Img<T> sourceImage = ImgView.wrap(parentsWithChildren.get(0).getSourceImage(), new ArrayImgFactory(new FloatType()));
 
         RandomAccessibleInterval<BitType> parentsMask = getParentsMask(parentsWithChildren);
         HashMap<Integer, SimpleComponent<T>> childLabelToComponentMap = new HashMap<>();
         ImgLabeling<Integer, IntType> childLabeling = getChildLabeling(parentsWithChildren, childLabelToComponentMap);
-        childLabeling = erodeLabels(childLabeling, sourceImage);
+        childLabeling = erodeLabels(childLabeling);
         ImgLabeling<Integer, IntType> out = doWatershed(sourceImage, childLabeling, parentsMask);
         LabelRegions<Integer> regions = new LabelRegions<>(out);
 
@@ -85,170 +79,105 @@ public class RecursiveComponentWatershedder<T extends Type<T>, C extends Compone
             SimpleComponent<T> child = childLabelToComponentMap.get(label);
             child.setRegion(region);
         }
-//
-//
-//        ///////////////////////////////////////////////////////////////////////////////////////////////
-//
-//        List<SimpleComponent<T>> children = parent.getChildren();
-//        if (children.size() == 0) return; // no children to watershed
-//
-//        Img<T> sourceImage = ImgView.wrap(parent.getSourceImage(), new ArrayImgFactory(new FloatType()));
-//        ImgLabeling<Integer, IntType> parentLabeling = createLabelingImage(sourceImage);
-//        ImgLabeling<Integer, IntType> childLabeling = createLabelingImage(sourceImage);
-//
-//        Integer label = 1;
-//        parent.writeLabels(parentLabeling, label);
-//        for(SimpleComponent<T> child: children){
-//            child.writeLabels(childLabeling, label);
-//            ++label;
-//        }
-//        childLabeling = erodeLabels(childLabeling, sourceImage);
-//        RandomAccessibleInterval<BitType> mask = ops.convert().bit(Views.iterable(parentLabeling.getIndexImg()));
-//
-////        ImageJFunctions.show(mask, "mask");
-////        ImageJFunctions.show(childLabeling.getIndexImg(), "merged child image");
-//
-//        ImgLabeling<Integer, IntType> out = doWatershed(sourceImage, childLabeling, mask);
-//        LabelRegions<Integer> regions = new LabelRegions<>(out);
-//
-//        label = 1;
-//        for(SimpleComponent<T> child: children){
-//            LabelRegion<Integer> region = regions.getLabelRegion(label);
-//            child.setRegion(region);
-//            ++label;
-//        }
     }
 
-    public SimpleComponentTree<T, C> recursivelyWatershedComponents(SimpleComponentTree<T, C> tree) {
+    /**
+     * Returns a mask of the parent components in {@param parentComponents}, which is used for the masked watershedding.
+     *
+     * @param parentComponents
+     * @return
+     */
+    @NotNull
+    private RandomAccessibleInterval<BitType> getParentsMask(List<SimpleComponent<T>> parentComponents) {
+        Img<T> sourceImage = ImgView.wrap(parentComponents.get(0).getSourceImage(), new ArrayImgFactory(new FloatType()));
+        ImgLabeling<Integer, IntType> parentLabeling = createLabelingImage(sourceImage);
+        Integer label = 1;
+        for (SimpleComponent<T> parent : parentComponents) { /* write to labeling image */
+            parent.writeLabels(parentLabeling, label);
+        }
+        RandomAccessibleInterval<BitType> mask = ops.convert().bit(Views.iterable(parentLabeling.getIndexImg())); /* convert labeling to mask*/
+        return mask;
+    }
 
-        Consumer<Pair<List<SimpleComponent<T>>, Integer>> levelComponentsConsumer = (levelComponentsListAndLevel)-> {
-            List<SimpleComponent<T>> componentsOfLevel = levelComponentsListAndLevel.getValue0();
-            {
-                watershedChildrenIntoThisLevel(componentsOfLevel);
+    /**
+     * Generates a labelling for all children components of the parent components in {@param parents}.
+     * {@param childLabelToComponentMap} holds a mapping of child components to their corresponding label.
+     *
+     * @param parents
+     * @param childLabelToComponentMap
+     * @return
+     */
+    @NotNull
+    private ImgLabeling<Integer, IntType> getChildLabeling(List<SimpleComponent<T>> parents, HashMap<Integer, SimpleComponent<T>> childLabelToComponentMap) {
+        Img<T> sourceImage = ImgView.wrap(parents.get(0).getSourceImage(), new ArrayImgFactory(new FloatType()));
+        ImgLabeling<Integer, IntType> childLabeling = createLabelingImage(sourceImage);
+        Integer childLabel = 1;
+        for (SimpleComponent<T> parent : parents) {
+            List<SimpleComponent<T>> children = parent.getChildren();
+            for (SimpleComponent<T> child : children) {
+                child.writeLabels(childLabeling, childLabel);
+                childLabelToComponentMap.put(childLabel, child);
+                ++childLabel;
             }
-        };
-
-        // run for components in each level
-        ComponentTreeUtils.doForEachComponentInTreeLevel(tree, levelComponentsConsumer);
-//
-//
-//        for (final SimpleComponent root : tree.roots()) {
-//            RecursivelyWatershed(root);
-//        }
-//        return tree;
-//        throw new NotImplementedException();
-        return tree;
+        }
+        return childLabeling;
     }
 
-    private ImgLabeling<Integer, IntType> erodeLabels(ImgLabeling<Integer, IntType> labeling, RandomAccessibleInterval<T> sourceImage){
-        ImgLabeling<Integer, IntType> labelingEroded = createLabelingImage(sourceImage);
+    /**
+     * This method erodes the child labeling. This is necessary, because there is a bug in ops.image().watershed, which
+     * fails, if markers partially overlap with the border of a masked area (here: the region inside the parent mask).
+     * When this bug is fixed, then this eroding of labels should not be needed anymore.
+     *
+     * @param labeling
+     * @return
+     */
+    private ImgLabeling<Integer, IntType> erodeLabels(ImgLabeling<Integer, IntType> labeling) {
+        ImgLabeling<Integer, IntType> labelingEroded = createLabelingImage(labeling.getIndexImg());
         RandomAccessibleInterval<IntType> backingImage = labeling.getIndexImg();
         backingImage = (RandomAccessibleInterval) ops.morphology().erode(backingImage, new RectangleShape(1, false));
         Cursor<LabelingType<Integer>> labelingErodedCursor = labelingEroded.cursor();
         Cursor<LabelingType<Integer>> labelingCursor = labeling.cursor();
         RandomAccess<IntType> rndAcc = backingImage.randomAccess();
 
-        while(labelingErodedCursor.hasNext()){
+        while (labelingErodedCursor.hasNext()) {
             labelingErodedCursor.fwd();
             labelingCursor.fwd();
             rndAcc.setPosition(labelingErodedCursor);
-            if(rndAcc.get().get() != 0) {
+            if (rndAcc.get().get() != 0) {
                 labelingErodedCursor.get().set(labelingCursor.get().copy());
             }
         }
-
-//        ImageJFunctions.show(labeling.getIndexImg(), "eroded");
-//        ImageJFunctions.show(labelingOrig.getIndexImg(), "non-eroded");
-
         return labelingEroded;
     }
 
-    private void RecursivelyWatershed(SimpleComponent<T> parent) {
-        List<SimpleComponent<T>> children = parent.getChildren();
-        if (children.size() == 0) return; // no children to watershed
-
-        Img<T> sourceImage = ImgView.wrap(parent.getSourceImage(), new ArrayImgFactory(new FloatType()));
-        ImgLabeling<Integer, IntType> parentLabeling = createLabelingImage(sourceImage);
-        ImgLabeling<Integer, IntType> childLabeling = createLabelingImage(sourceImage);
-
-        Integer label = 1;
-        parent.writeLabels(parentLabeling, label);
-        for(SimpleComponent<T> child: children){
-            child.writeLabels(childLabeling, label);
-            ++label;
-        }
-        childLabeling = erodeLabels(childLabeling, sourceImage);
-        RandomAccessibleInterval<BitType> mask = ops.convert().bit(Views.iterable(parentLabeling.getIndexImg()));
-
-//        ImageJFunctions.show(mask, "mask");
-//        ImageJFunctions.show(childLabeling.getIndexImg(), "merged child image");
-
-        ImgLabeling<Integer, IntType> out = doWatershed(sourceImage, childLabeling, mask);
-        LabelRegions<Integer> regions = new LabelRegions<>(out);
-
-
-
-        // TODO: THIS NEEDS TO BE FIXED, TO MAKE SURE THE LABELLING FROM THE WATERSHEDDING CORRESPONDS TO THE LABELING FROM THE MARKER-IMAGE AND CORRESPONDING COMPONENTS
-        label = 1;
-        for(SimpleComponent<T> child: children){
-            LabelRegion<Integer> region = regions.getLabelRegion(label);
-            child.setRegion(region);
-            ++label;
-        }
-
-        for(SimpleComponent<T> child: children){
-            RecursivelyWatershed(child);
-        }
-
-//        ImageJFunctions.show((Img) sourceImage);
-//        ImageJFunctions.show(childLabeling.getIndexImg());
-//        ImageJFunctions.show(mask);
-//        ImageJFunctions.show(out.getIndexImg());
-
-//        printRegionSizes(childLabeling);
-//        System.out.println("------");
-//        printRegionSizes(out);
-
-    }
-
-    private ImgLabeling<Integer, IntType> createLabelingImage(RandomAccessibleInterval<T> sourceImage){
+    /**
+     * Convenience method for creating a labeling image with same dimensions as the source image.
+     *
+     * @param sourceImage
+     * @return
+     */
+    private ImgLabeling<Integer, IntType> createLabelingImage(RandomAccessibleInterval sourceImage) {
         long[] dims = new long[sourceImage.numDimensions()];
         sourceImage.dimensions(dims);
         Img<IntType> img = ArrayImgs.ints(dims);
         return new ImgLabeling<>(img);
     }
 
-    private ImgLabeling<Integer, IntType> doWatershed(final Img in, final ImgLabeling<Integer, IntType> markers, RandomAccessibleInterval<BitType> mask) {
-//        return (ImgLabeling<Integer, IntType>) ops.run(WatershedSeeded.class, null, in,
-//                markers, true, false, mask);
-        long[] dims = new long[in.numDimensions()];
-        in.dimensions(dims);
-        Img<IntType> img = ArrayImgs.ints(dims);
-        ImgLabeling<Integer, IntType> res = new ImgLabeling<Integer, IntType>(img);
-        Img<FloatType> input = in.factory().create(in); //  QUESTION: if I use generic Img<t> here, the compiler issues a warning. Why?!
-        ops.image().invert(input, in);
-        return ops.image().watershed(null, input, markers, false, false, mask);
-//        try
-//        {
-//        res = ops.image().watershed(null, input, markers, false, false, mask);
-//            return res;
-//        }
-//        catch(java.util.NoSuchElementException err)
-//        {
-//            ImageJFunctions.show(in);
-//            ImageJFunctions.show(markers.getSource());
-//            ImageJFunctions.show(mask);
-//            return res;
-//        }
-    }
-
-    private void printRegionSizes(ImgLabeling<Integer, IntType> seedLabels) {
-        LabelRegions<Integer> regions = new LabelRegions<>(seedLabels);
-        Iterator<LabelRegion<Integer>> regionIterator = regions.iterator();
-        while (regionIterator.hasNext()) {
-            LabelRegion<Integer> region = regionIterator.next();
-            System.out.println(String.format("label: %d", region.getLabel()));
-            System.out.println(String.format("region size: %d", region.size()));
-        }
+    /**
+     * Perform the watershedding on the sourceImage using the parent-components as masks and the (eroded) child-components
+     * as markers. We invert sourceImage, so that the locations of the cells (which have value 1) become the basins for
+     * the watershedding.
+     *
+     * @param sourceImage
+     * @param markers
+     * @param mask
+     * @return
+     */
+    private ImgLabeling<Integer, IntType> doWatershed(final Img sourceImage, final ImgLabeling<Integer, IntType> markers, RandomAccessibleInterval<BitType> mask) {
+        long[] dims = new long[sourceImage.numDimensions()];
+        sourceImage.dimensions(dims);
+        Img<FloatType> invertedSourceImage = sourceImage.factory().create(sourceImage);
+        ops.image().invert(invertedSourceImage, sourceImage); // invert the intensities of the probability maps, so that cell-areas are watershed basins
+        return ops.image().watershed(null, invertedSourceImage, markers, false, false, mask);
     }
 }

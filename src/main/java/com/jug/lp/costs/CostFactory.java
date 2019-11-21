0e019e24fc9967765d7468acbc3c952b93f12dcb
Michael Mell <michael.mell@gmx.net>
2019-11-21 09:58:27 +0100

    Adjust parameters determining the cost-crossover for exiting components.

--- src/main/java/com/jug/lp/costs/CostFactory.java
+++ src/main/java/com/jug/lp/costs/CostFactory.java
@@ -87,8 +87,8 @@ public class CostFactory {
         float top_roi_boundary = (float) MoMA.GL_OFFSET_TOP;
         double verticalPosition = ((SimpleComponent<FloatType>) component).firstMomentPixelCoordinates()[1];
         double distanceFromBoundary = top_roi_boundary - verticalPosition;
-        double sigma = 2; // defines the range, over which the cost increases.
-        double maximumCost = 2; // maximum cost possible for a component above the boundary
+        double sigma = 4; // defines the range, over which the cost increases.
+        double maximumCost = 5; // maximum cost possible for a component above the boundary
         double minimumCost = -1; // minimum cost of a component below the boundary
         float cost = (float) (minimumCost + (-minimumCost + maximumCost)/(1 + Math.exp(-distanceFromBoundary/sigma)));
 		return cost;

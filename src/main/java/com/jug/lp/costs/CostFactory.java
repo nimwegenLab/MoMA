4f2337a2d888825ceabc5e3feac847ad575e5388
Michael Mell <michael.mell@gmx.net>
2020-01-06 15:00:34 +0100

    Reduce max. cost for component leaving ROI to 1, because it reduces the risk of violating the continuity constraint.

--- src/main/java/com/jug/lp/costs/CostFactory.java
+++ src/main/java/com/jug/lp/costs/CostFactory.java
@@ -88,7 +88,7 @@ public class CostFactory {
         double verticalPosition = ((SimpleComponent<FloatType>) component).firstMomentPixelCoordinates()[1];
         double distanceFromBoundary = top_roi_boundary - verticalPosition;
         double sigma = 4; // defines the range, over which the cost increases.
-        double maximumCost = 5; // maximum cost possible for a component above the boundary
+        double maximumCost = 1; // maximum cost possible for a component above the boundary
         double minimumCost = -1; // minimum cost of a component below the boundary
         float cost = (float) (minimumCost + (-minimumCost + maximumCost)/(1 + Math.exp(-distanceFromBoundary/sigma)));
 		return cost;

fc0912cb7b86496c896b3648c425af0595bcb4ae
Michael Mell <michael.mell@gmx.net>
2020-03-23 15:16:35 +0100

    Add documentation.

--- src/main/java/com/jug/export/ResultTable.java
+++ src/main/java/com/jug/export/ResultTable.java
@@ -5,21 +5,45 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for outputting data columns to a CSV file.
+ * To be used in conjunction with {@link ResultTableColumn}, which holds
+ * the values of individual columns.
+ */
 public class ResultTable {
     List<ResultTableColumn> columnList = new ArrayList<>();
     String separator = ";";
 
+    /**
+     * Write table to {@param writer}.
+     *
+     * @param writer output writer
+     * @throws IOException thrown by {@param writer}
+     */
     public void print(Writer writer) throws IOException {
         printHeader(writer);
         printRows(writer);
         writer.flush();
     }
 
+    /**
+     * Add a column to this table.
+     *
+     * @param column to add
+     * @param <T> value type contained in the column
+     * @return a reference to the added column, so that we can keep it for adding values.
+     */
     public <T> ResultTableColumn<T> addColumn(ResultTableColumn<T> column) {
         columnList.add(column);
         return column;
     }
 
+    /**
+     * Write header to the output writer.
+     *
+     * @param writer output writer
+     * @throws IOException thrown by {@param writer}
+     */
     private void printHeader(Writer writer) throws IOException {
         for (ResultTableColumn column : columnList) {
             column.writeHeader(writer);
@@ -28,6 +52,12 @@ public class ResultTable {
         writer.write("\n");
     }
 
+    /**
+     * Write table rows to {@param writer}
+     *
+     * @param writer output writer
+     * @throws IOException thrown by writer
+     */
     private void printRows(Writer writer) throws IOException {
         int numberOfEntries = columnList.get(0).getNumberOfEntries();
 
@@ -40,6 +70,12 @@ public class ResultTable {
         }
     }
 
+    /**
+     * Write the column separator.
+     *
+     * @param writer output writer
+     * @throws IOException thrown by writer
+     */
     private void writeSeparator(Writer writer) throws IOException {
         writer.write(String.format("%s", separator));
     }

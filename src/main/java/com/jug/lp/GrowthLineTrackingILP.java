ac149181d80d8ebab0eb9897278a9bd7e595738a
Michael Mell <michael.mell@gmx.net>
2020-04-02 11:16:16 +0200

    Orthography.

--- src/main/java/com/jug/lp/GrowthLineTrackingILP.java
+++ src/main/java/com/jug/lp/GrowthLineTrackingILP.java
@@ -941,7 +941,7 @@ public class GrowthLineTrackingILP {
 					ret.add( hyp );
 				}
 			} catch ( final GRBException e ) {
-				System.err.println( "ERROR: It could not be determined of a certain assignment was chosen during the convex optimization!" );
+				System.err.println( "ERROR: It could not be determined if a certain assignment was chosen during the convex optimization!" );
 				e.printStackTrace();
 			}
 		}

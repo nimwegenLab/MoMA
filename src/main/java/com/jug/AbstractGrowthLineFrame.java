cca12383fb462cb8001c99420c6539a697aa37ef
Michael Mell <michael.mell@gmx.net>
2020-04-02 16:47:32 +0200

    Cleanup.

--- src/main/java/com/jug/AbstractGrowthLineFrame.java
+++ src/main/java/com/jug/AbstractGrowthLineFrame.java
@@ -22,260 +22,244 @@ import java.util.Vector;
 
 /**
  * @author jug
- *         Represents one growth line (well) in which Bacteria can grow, at one
- *         instance in time.
- *         This corresponds to one growth line micrograph. The class
- *         representing an entire time
- *         series (2d+t) representation of an growth line is
- *         <code>GrowthLine</code>.
+ * Represents one growth line (well) in which Bacteria can grow, at one
+ * instance in time.
+ * This corresponds to one growth line micrograph. The class
+ * representing an entire time
+ * series (2d+t) representation of an growth line is
+ * <code>GrowthLine</code>.
  */
-public abstract class AbstractGrowthLineFrame< C extends Component< FloatType, C > > {
+public abstract class AbstractGrowthLineFrame<C extends Component<FloatType, C>> {
 
-	// -------------------------------------------------------------------------------------
-	// private fields
-	// -------------------------------------------------------------------------------------
-	/**
-	 * Points at all the detected GrowthLine centers associated with this
-	 * GrowthLine.
-	 */
-	private GrowthLine parent;
-	private ComponentForest< C > componentTree;
-	private Img<FloatType> image;
+    // -------------------------------------------------------------------------------------
+    // private fields
+    // -------------------------------------------------------------------------------------
+    /**
+     * Points at all the detected GrowthLine centers associated with this
+     * GrowthLine.
+     */
+    private GrowthLine parent;
+    private ComponentForest<C> componentTree;
+    private Img<FloatType> image;
 
-	// -------------------------------------------------------------------------------------
-	// setters and getters
-	// -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------
+    // setters and getters
+    // -------------------------------------------------------------------------------------
 
-    public void setImage(final Img<FloatType> image){
-        this.image = image;
+    // -------------------------------------------------------------------------------------
+    // constructors
+    // -------------------------------------------------------------------------------------
+    AbstractGrowthLineFrame() {
     }
 
-    public Img<FloatType> getImage(){
+    public Img<FloatType> getImage() {
         return image.copy();
     }
 
-	/**
-	 * @return the growth line time series this one growth line is part of.
-	 */
-	public GrowthLine getParent() {
-		return parent;
-	}
-
-	/**
-	 * @param parent
-	 *            - the growth line time series this one growth line is part of.
-	 */
-	public void setParent( final GrowthLine parent ) {
-		this.parent = parent;
-	}
-
-	/**
-	 * @return the componentTree
-	 */
-	public ComponentForest< C > getComponentTree() { // MM-2019-06-10: This should probably be called getComponentForest?!
-		return componentTree;
-	}
-
-	/**
-	 * @return the x-offset of the GrowthLineFrame given the original micrograph
-	 */
-	public long getOffsetX() {
-	    return image.dimension(0)/2;
+    public void setImage(final Img<FloatType> image) {
+        this.image = image;
     }
 
-	/**
-	 * @return the y-offset of the GrowthLineFrame given the original micrograph
-	 */
-	public long getOffsetY() {
-		return 0;
-	}
+    /**
+     * @return the growth line time series this one growth line is part of.
+     */
+    public GrowthLine getParent() {
+        return parent;
+    }
 
-	/**
-	 * @return the f-offset of the GrowthLineFrame given the original micrograph
-	 *         (stack)
-	 */
-	public long getOffsetF() {
-		return parent.getFrames().indexOf( this );
-	}
+    /**
+     * @param parent - the growth line time series this one growth line is part of.
+     */
+    public void setParent(final GrowthLine parent) {
+        this.parent = parent;
+    }
 
-	// -------------------------------------------------------------------------------------
-	// constructors
-	// -------------------------------------------------------------------------------------
-	AbstractGrowthLineFrame() {}
+    /**
+     * @return the componentTree
+     */
+    public ComponentForest<C> getComponentTree() { // MM-2019-06-10: This should probably be called getComponentForest?!
+        return componentTree;
+    }
 
-	// -------------------------------------------------------------------------------------
-	// methods
-	// -------------------------------------------------------------------------------------
+    /**
+     * @return the x-offset of the GrowthLineFrame given the original micrograph
+     */
+    public long getOffsetX() {
+        return image.dimension(0) / 2;
+    }
 
+    /**
+     * @return the y-offset of the GrowthLineFrame given the original micrograph
+     */
+    public long getOffsetY() {
+        return 0;
+    }
 
-	/**
-	 * Using the imglib2 component tree to find the most stable components
-	 * (bacteria).
-	 */
-	public void generateSimpleSegmentationHypotheses(final Img<FloatType> img, int frameIndex) {
-		IntervalView<FloatType> currentImage = Views.hyperSlice(img, 2, frameIndex);
-		componentTree = buildIntensityTree(currentImage);
-	}
+    /**
+     * @return the f-offset of the GrowthLineFrame given the original micrograph
+     * (stack)
+     */
+    public long getOffsetF() {
+        return parent.getFrames().indexOf(this);
+    }
 
-	/**
-	 * Using the imglib2 component tree to find the most stable components
-	 * (bacteria).
-	 */
-	protected abstract ComponentForest< C > buildIntensityTree( final RandomAccessibleInterval< FloatType > raiFkt );
+    // -------------------------------------------------------------------------------------
+    // methods
+    // -------------------------------------------------------------------------------------
 
-	/**
-	 * Draws the optimal segmentation (determined by the solved ILP) into the
-	 * given <code>Img</code>.
-	 *
-	 * @param img
-	 *            the Img to draw into.
-	 * @param view
-	 *            the active view on that Img (in order to know the pixel
-	 *            offsets)
-	 * @param optimalSegmentation
-	 *            a <code>List</code> of the hypotheses containing
-	 *            component-tree-nodes that represent the optimal segmentation
-	 *            (the one returned by the solution to the ILP).
-	 */
-	public void drawOptimalSegmentation( final Img< ARGBType > img, final IntervalView< FloatType > view, final List< Hypothesis< Component< FloatType, ? >>> optimalSegmentation ) {
-		final RandomAccess< ARGBType > raAnnotationImg = img.randomAccess();
-		long offsetX = view.min( 0 );
-		long offsetY = view.min( 1 );
-		for ( final Hypothesis< Component< FloatType, ? >> hyp : optimalSegmentation ) {
-			final Component< FloatType, ? > ctn = hyp.getWrappedComponent();
-			if ( hyp.isPruned() ) {
-				ArgbDrawingUtils.taintPrunedComponentTreeNode(
-						ctn,
-						raAnnotationImg,
-						offsetX,
-						offsetY );
-			} else if ( hyp.getSegmentSpecificConstraint() != null ) {
-				ArgbDrawingUtils.taintForcedComponentTreeNode(
-						ctn,
-						raAnnotationImg,
-						offsetX,
-						offsetY );
-			} else {
-				ArgbDrawingUtils.taintOptimalComponentTreeNode(
-						ctn,
-						raAnnotationImg,
-						offsetX,
-						offsetY );
-			}
-		}
-	}
+    /**
+     * Using the imglib2 component tree to find the most stable components
+     * (bacteria).
+     */
+    public void generateSimpleSegmentationHypotheses(final Img<FloatType> img, int frameIndex) {
+        IntervalView<FloatType> currentImage = Views.hyperSlice(img, 2, frameIndex);
+        componentTree = buildIntensityTree(currentImage);
+    }
 
-	public void drawOptionalSegmentation( final Img< ARGBType > img, final IntervalView< FloatType > view, final Component< FloatType, ? > optionalSegmentation ) {
-		final RandomAccess< ARGBType > raAnnotationImg = img.randomAccess();
-		long offsetX = view.min( 0 );
-		long offsetY = view.min( 1 );
-		ArgbDrawingUtils.taintInactiveComponentTreeNode( optionalSegmentation, raAnnotationImg, offsetX, offsetY );
-	}
+    /**
+     * Using the imglib2 component tree to find the most stable components
+     * (bacteria).
+     */
+    protected abstract ComponentForest<C> buildIntensityTree(final RandomAccessibleInterval<FloatType> raiFkt);
 
-	/**
-	 * @return the average X coordinate of the center line of this
-	 *         <code>GrowthLine</code>
-	 */
-	public int getAvgXpos() {
-		return (int) getOffsetX();
-	}
+    /**
+     * Draws the optimal segmentation (determined by the solved ILP) into the
+     * given <code>Img</code>.
+     *
+     * @param img                 the Img to draw into.
+     * @param view                the active view on that Img (in order to know the pixel
+     *                            offsets)
+     * @param optimalSegmentation a <code>List</code> of the hypotheses containing
+     *                            component-tree-nodes that represent the optimal segmentation
+     *                            (the one returned by the solution to the ILP).
+     */
+    public void drawOptimalSegmentation(final Img<ARGBType> img, final IntervalView<FloatType> view, final List<Hypothesis<Component<FloatType, ?>>> optimalSegmentation) {
+        final RandomAccess<ARGBType> raAnnotationImg = img.randomAccess();
+        long offsetX = view.min(0);
+        long offsetY = view.min(1);
+        for (final Hypothesis<Component<FloatType, ?>> hyp : optimalSegmentation) {
+            final Component<FloatType, ?> ctn = hyp.getWrappedComponent();
+            if (hyp.isPruned()) {
+                ArgbDrawingUtils.taintPrunedComponentTreeNode(ctn, raAnnotationImg, offsetX, offsetY);
+            } else if (hyp.getSegmentSpecificConstraint() != null) {
+                ArgbDrawingUtils.taintForcedComponentTreeNode(ctn, raAnnotationImg, offsetX, offsetY);
+            } else {
+                ArgbDrawingUtils.taintOptimalComponentTreeNode(ctn, raAnnotationImg, offsetX, offsetY);
+            }
+        }
+    }
 
-	/**
-	 * @return the time-step this GLF corresponds to in the GL it is part of.
-	 */
-	public int getTime() {
-		return this.getParent().getFrames().indexOf( this );
-	}
+    public void drawOptionalSegmentation(final Img<ARGBType> img, final IntervalView<FloatType> view, final Component<FloatType, ?> optionalSegmentation) {
+        final RandomAccess<ARGBType> raAnnotationImg = img.randomAccess();
+        long offsetX = view.min(0);
+        long offsetY = view.min(1);
+        ArgbDrawingUtils.taintInactiveComponentTreeNode(optionalSegmentation, raAnnotationImg, offsetX, offsetY);
+    }
 
-	/**
-	 * Returns the number of cells in this GLF.
-	 */
-	public int getSolutionStats_numberOfTrackedCells() {
-		int cells = 0;
-		final GrowthLineTrackingILP ilp = getParent().getIlp();
-		for ( final Set< AbstractAssignment< Hypothesis< Component< FloatType, ? >>> > set : ilp.getOptimalRightAssignments( this.getTime() ).values() ) {
-			for ( final AbstractAssignment< Hypothesis< Component< FloatType, ? >>> ora : set ) {
-				cells++;
-			}
-		}
-		return cells;
-	}
+    /**
+     * @return the average X coordinate of the center line of this
+     * <code>GrowthLine</code>
+     */
+    public int getAvgXpos() {
+        return (int) getOffsetX();
+    }
 
-	/**
-	 * Returns the rank of the given hypothesis {@param hyp} in the GL.
-	 *
-	 * @param hyp Hypothesis for which the rank will be determined
-	 * @return The lower-most segmented cell returns 0. For each active
-	 *         segment with center above {@param hyp} the return value is
-	 *         increased by 1.
-	 */
-	public int getSolutionStats_cellRank(final Hypothesis< Component< FloatType, ? >> hyp ) {
-		int pos = 0;
+    /**
+     * @return the time-step this GLF corresponds to in the GL it is part of.
+     */
+    public int getTime() {
+        return this.getParent().getFrames().indexOf(this);
+    }
 
-		final GrowthLineTrackingILP ilp = getParent().getIlp();
-		for ( final Set< AbstractAssignment< Hypothesis< Component< FloatType, ? >>> > optRightAssmnt : ilp.getOptimalRightAssignments(
-				this.getTime() ).values() ) {
+    /**
+     * Returns the number of cells in this GLF.
+     */
+    public int getSolutionStats_numberOfTrackedCells() {
+        int cells = 0;
+        final GrowthLineTrackingILP ilp = getParent().getIlp();
+        for (final Set<AbstractAssignment<Hypothesis<Component<FloatType, ?>>>> set : ilp.getOptimalRightAssignments(this.getTime()).values()) {
+            for (final AbstractAssignment<Hypothesis<Component<FloatType, ?>>> ora : set) {
+                cells++;
+            }
+        }
+        return cells;
+    }
 
-			for ( final AbstractAssignment< Hypothesis< Component< FloatType, ? >>> ora : optRightAssmnt ) {
-				Hypothesis< Component< FloatType, ? >> srcHyp = null;
-				if ( ora instanceof MappingAssignment ) {
-					srcHyp = ( ( MappingAssignment ) ora ).getSourceHypothesis();
-				}
-				if ( ora instanceof DivisionAssignment ) {
-					srcHyp = ( ( DivisionAssignment ) ora ).getSourceHypothesis();
-				}
-				if ( ora instanceof ExitAssignment ) {
-					srcHyp = ( ( ExitAssignment ) ora ).getAssociatedHypothesis();
-				}
-				if ( srcHyp != null ) {
-					if (ComponentTreeUtils.isAbove(hyp, srcHyp)) {
-						pos++;
-					}
-				}
-			}
-		}
-		return pos;
-	}
+    /**
+     * Returns the rank of the given hypothesis {@param hyp} in the GL.
+     *
+     * @param hyp Hypothesis for which the rank will be determined
+     * @return The lower-most segmented cell returns 0. For each active
+     * segment with center above {@param hyp} the return value is
+     * increased by 1.
+     */
+    public int getSolutionStats_cellRank(final Hypothesis<Component<FloatType, ?>> hyp) {
+        int pos = 0;
 
-	public Vector< ValuePair< ValuePair< Integer, Integer >, ValuePair< Integer, Integer > >> getSolutionStats_limitsAndRightAssType() {
-		final Vector< ValuePair< ValuePair< Integer, Integer >, ValuePair< Integer, Integer > >> ret = new Vector<>();
-		for ( final Hypothesis< Component< FloatType, ? > > hyp : getParent().getIlp().getOptimalRightAssignments( this.getTime() ).keySet() ) {
+        final GrowthLineTrackingILP ilp = getParent().getIlp();
+        for (final Set<AbstractAssignment<Hypothesis<Component<FloatType, ?>>>> optRightAssmnt : ilp.getOptimalRightAssignments(
+                this.getTime()).values()) {
 
-			final AbstractAssignment< Hypothesis< Component< FloatType, ? >>> aa = getParent().getIlp().getOptimalRightAssignments( this.getTime() ).get( hyp ).iterator().next();
+            for (final AbstractAssignment<Hypothesis<Component<FloatType, ?>>> ora : optRightAssmnt) {
+                Hypothesis<Component<FloatType, ?>> srcHyp = null;
+                if (ora instanceof MappingAssignment) {
+                    srcHyp = ((MappingAssignment) ora).getSourceHypothesis();
+                }
+                if (ora instanceof DivisionAssignment) {
+                    srcHyp = ((DivisionAssignment) ora).getSourceHypothesis();
+                }
+                if (ora instanceof ExitAssignment) {
+                    srcHyp = ((ExitAssignment) ora).getAssociatedHypothesis();
+                }
+                if (srcHyp != null) {
+                    if (ComponentTreeUtils.isAbove(hyp, srcHyp)) {
+                        pos++;
+                    }
+                }
+            }
+        }
+        return pos;
+    }
 
-			int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-			for (Localizable localizable : hyp.getWrappedComponent()) {
-				final int ypos = localizable.getIntPosition(0);
-				min = Math.min(min, ypos);
-				max = Math.max(max, ypos);
-			}
+    public Vector<ValuePair<ValuePair<Integer, Integer>, ValuePair<Integer, Integer>>> getSolutionStats_limitsAndRightAssType() {
+        final Vector<ValuePair<ValuePair<Integer, Integer>, ValuePair<Integer, Integer>>> ret = new Vector<>();
+        for (final Hypothesis<Component<FloatType, ?>> hyp : getParent().getIlp().getOptimalRightAssignments(this.getTime()).keySet()) {
 
-			ret.add(new ValuePair<>(new ValuePair<>(min, max), new ValuePair<>(aa.getType(), (aa.isGroundTruth() || aa.isGroundUntruth()) ? 1 : 0)) );
-		}
+            final AbstractAssignment<Hypothesis<Component<FloatType, ?>>> aa = getParent().getIlp().getOptimalRightAssignments(this.getTime()).get(hyp).iterator().next();
 
-		ret.sort(Comparator.comparing(o -> o.a.a));
-		return ret;
-	}
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (Localizable localizable : hyp.getWrappedComponent()) {
+                final int ypos = localizable.getIntPosition(0);
+                min = Math.min(min, ypos);
+                max = Math.max(max, ypos);
+            }
 
-	public Vector< ValuePair< Integer, Hypothesis< Component< FloatType, ? > >>> getSortedActiveHypsAndPos() {
-		final Vector< ValuePair< Integer, Hypothesis< Component< FloatType, ? > >>> positionedHyps = new Vector<>();
+            ret.add(new ValuePair<>(new ValuePair<>(min, max), new ValuePair<>(aa.getType(), (aa.isGroundTruth() || aa.isGroundUntruth()) ? 1 : 0)));
+        }
 
-		for ( final Hypothesis< Component< FloatType, ? > > hyp : getParent().getIlp().getOptimalRightAssignments( this.getTime() ).keySet() ) {
-			// find out where this hypothesis is located along the GL
-			int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-			for (Localizable localizable : hyp.getWrappedComponent()) {
-				final int ypos = localizable.getIntPosition(0);
-				min = Math.min(min, ypos);
-				max = Math.max(max, ypos);
-			}
+        ret.sort(Comparator.comparing(o -> o.a.a));
+        return ret;
+    }
 
-			if ( !hyp.isPruned() ) {
-				positionedHyps.add(new ValuePair<>(-max, hyp) );
-			}
-		}
+    public Vector<ValuePair<Integer, Hypothesis<Component<FloatType, ?>>>> getSortedActiveHypsAndPos() {
+        final Vector<ValuePair<Integer, Hypothesis<Component<FloatType, ?>>>> positionedHyps = new Vector<>();
 
-		positionedHyps.sort(Comparator.comparing(o -> o.a));
+        for (final Hypothesis<Component<FloatType, ?>> hyp : getParent().getIlp().getOptimalRightAssignments(this.getTime()).keySet()) {
+            // find out where this hypothesis is located along the GL
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (Localizable localizable : hyp.getWrappedComponent()) {
+                final int ypos = localizable.getIntPosition(0);
+                min = Math.min(min, ypos);
+                max = Math.max(max, ypos);
+            }
 
-		return positionedHyps;
-	}
+            if (!hyp.isPruned()) {
+                positionedHyps.add(new ValuePair<>(-max, hyp));
+            }
+        }
+
+        positionedHyps.sort(Comparator.comparing(o -> o.a));
+
+        return positionedHyps;
+    }
 }

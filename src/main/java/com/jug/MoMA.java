b0300fa46b6bda97e20558c144a3528c144a1bae
Michael Mell <michael.mell@unibas.ch>
2021-09-13 17:11:13 +0200

    WIP: Start work on refactoring all config parameters into a separate class out of MoMA.java.

--- src/main/java/com/jug/MoMA.java
+++ src/main/java/com/jug/MoMA.java
@@ -1,5 +1,6 @@
 package com.jug;
 
+import com.jug.config.ConfigurationManager;
 import com.jug.datahandling.IImageProvider;
 import com.jug.gui.MoMAGui;
 import com.jug.gui.MoMAModel;
@@ -38,6 +39,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import static com.jug.config.ConfigurationManager.DEFAULT_GUI_POS_X;
 import static com.jug.development.featureflags.FeatureFlags.featureFlagDisableMaxCellDrop;
 import static org.apache.commons.io.FilenameUtils.removeExtension;
 
@@ -77,15 +79,15 @@ public class MoMA implements IImageProvider {
 	 * Parameter: sigma for gaussian blurring in x-direction of the raw image
 	 * data. Used while searching the growth line centers.
 	 */
-	private static float SIGMA_GL_DETECTION_X = 20f;
-	private static float SIGMA_GL_DETECTION_Y = 0f;
+	public static float SIGMA_GL_DETECTION_X = 20f;
+	public static float SIGMA_GL_DETECTION_Y = 0f;
 
 	/**
 	 * Parameter: sigma for gaussian blurring in x-direction of the raw image
 	 * data. Used while searching the gaps between bacteria.
 	 */
-	private static float SIGMA_PRE_SEGMENTATION_X = 0f;
-	private static float SIGMA_PRE_SEGMENTATION_Y = 0f;
+	public static float SIGMA_PRE_SEGMENTATION_X = 0f;
+	public static float SIGMA_PRE_SEGMENTATION_Y = 0f;
 
 	/**
 	 * Parameter: how many pixels wide is the image containing the selected
@@ -101,7 +103,7 @@ public class MoMA implements IImageProvider {
 	 * the left and right image border will be neglected. Reason: detection not
 	 * reliable if well is truncated.
 	 */
-	private static int GL_OFFSET_LATERAL = 20;
+	public static int GL_OFFSET_LATERAL = 20;
 
 	/**
 	 * Vertical center position on which the exit range defined with {@link COMPONENT_EXIT_RANGE} is centered.
@@ -119,20 +121,20 @@ public class MoMA implements IImageProvider {
 	 * take the background intensities from that will be subtracted from the
 	 * growth line.
 	 */
-	private static int BGREM_TEMPLATE_XMAX = 35;
+	public static int BGREM_TEMPLATE_XMAX = 35;
 
 	/**
 	 * Minimum offset in x direction (with respect to growth line center) to
 	 * take the background intensities from that will be subtracted from the
 	 * growth line.
 	 */
-	private static int BGREM_TEMPLATE_XMIN = 20;
+	public static int BGREM_TEMPLATE_XMIN = 20;
 
 	/**
 	 * Offsets in +- x direction (with respect to growth line center) where the
 	 * measured background values will be subtracted from.
 	 */
-	private static int BGREM_X_OFFSET = 35;
+	public static int BGREM_X_OFFSET = 35;
 
 	/**
 	 * Prior knowledge: minimal length of detected cells
@@ -142,7 +144,7 @@ public class MoMA implements IImageProvider {
 	/**
 	 * Prior knowledge: minimal contrast of a gap (also used for MSERs)
 	 */
-	private static float MIN_GAP_CONTRAST = 0.02f; // This is set to a very low
+	public static float MIN_GAP_CONTRAST = 0.02f; // This is set to a very low
 													// value that will basically
 													// not filter anything...
 	/**
@@ -152,7 +154,7 @@ public class MoMA implements IImageProvider {
 	 * classification is flat, the original (simple) mehod might still offer
 	 * some modulation!
 	 */
-	private static float SEGMENTATION_MIX_CT_INTO_PMFRF = 0.25f;
+	public static float SEGMENTATION_MIX_CT_INTO_PMFRF = 0.25f;
 
 	/**
 	 * String pointing at the weka-segmenter model file that should be used for
@@ -229,31 +231,31 @@ public class MoMA implements IImageProvider {
 	 * X-position of the main GUI-window. This value will be loaded from and
 	 * stored in the properties file!
 	 */
-	private static int GUI_POS_X;
+	public static int GUI_POS_X;
 
 	/**
 	 * Y-position of the main GUI-window. This value will be loaded from and
 	 * stored in the properties file!
 	 */
-	private static int GUI_POS_Y;
+	public static int GUI_POS_Y;
 
 	/**
 	 * Width (in pixels) of the main GUI-window. This value will be loaded from
 	 * and stored in the properties file!
 	 */
-	private static int GUI_WIDTH = 620;
+	public static int GUI_WIDTH = 620;
 
 	/**
 	 * Width (in pixels) of the main GUI-window. This value will be loaded from
 	 * and stored in the properties file!
 	 */
-	private static int GUI_HEIGHT = 740;
+	public static int GUI_HEIGHT = 740;
 
 	/**
 	 * Width (in pixels) of the console window. This value will be loaded from
 	 * and stored in the properties file!
 	 */
-	private static int GUI_CONSOLE_WIDTH = 600;
+	public static int GUI_CONSOLE_WIDTH = 600;
 
 
 	/**
@@ -298,7 +300,7 @@ public class MoMA implements IImageProvider {
 	/**
 	 * Property file in the moma directory the user.
 	 */
-	final File userMomaHomePropertyFile = new File(momaUserDirectory.getPath() + "/mm.properties");
+	private static final File userMomaHomePropertyFile = new File(momaUserDirectory.getPath() + "/mm.properties");
 
 	/**
 	 * Property file that is being used by this instance of Moma.
@@ -599,56 +601,57 @@ public class MoMA implements IImageProvider {
 
 		System.out.println( "VERSION: " + VERSION_STRING );
 
-		props = main.loadParams();
-		BGREM_TEMPLATE_XMIN = Integer.parseInt( props.getProperty( "BGREM_TEMPLATE_XMIN", Integer.toString( BGREM_TEMPLATE_XMIN ) ) );
-		BGREM_TEMPLATE_XMAX = Integer.parseInt( props.getProperty( "BGREM_TEMPLATE_XMAX", Integer.toString( BGREM_TEMPLATE_XMAX ) ) );
-		BGREM_X_OFFSET = Integer.parseInt( props.getProperty( "BGREM_X_OFFSET", Integer.toString( BGREM_X_OFFSET ) ) );
-		GL_WIDTH_IN_PIXELS = Integer.parseInt( props.getProperty( "GL_WIDTH_IN_PIXELS", Integer.toString( GL_WIDTH_IN_PIXELS ) ) );
-		MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS = Integer.parseInt( props.getProperty( "MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS", Integer.toString( MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS ) ) );
-		INTENSITY_FIT_RANGE_IN_PIXELS = Integer.parseInt( props.getProperty( "INTENSITY_FIT_RANGE_IN_PIXELS", Integer.toString( INTENSITY_FIT_RANGE_IN_PIXELS ) ) );
-		GL_OFFSET_TOP = Integer.parseInt( props.getProperty( "GL_OFFSET_TOP", Integer.toString( GL_OFFSET_TOP ) ) );
-		GL_OFFSET_LATERAL = Integer.parseInt( props.getProperty( "GL_OFFSET_LATERAL", Integer.toString( GL_OFFSET_LATERAL ) ) );
-		MIN_CELL_LENGTH = Integer.parseInt( props.getProperty( "MIN_CELL_LENGTH", Integer.toString( MIN_CELL_LENGTH ) ) );
-		MIN_GAP_CONTRAST = Float.parseFloat( props.getProperty( "MIN_GAP_CONTRAST", Float.toString( MIN_GAP_CONTRAST ) ) );
-		SIGMA_PRE_SEGMENTATION_X = Float.parseFloat( props.getProperty( "SIGMA_PRE_SEGMENTATION_X", Float.toString( SIGMA_PRE_SEGMENTATION_X ) ) );
-		SIGMA_PRE_SEGMENTATION_Y = Float.parseFloat( props.getProperty( "SIGMA_PRE_SEGMENTATION_Y", Float.toString( SIGMA_PRE_SEGMENTATION_Y ) ) );
-		SIGMA_GL_DETECTION_X = Float.parseFloat( props.getProperty( "SIGMA_GL_DETECTION_X", Float.toString( SIGMA_GL_DETECTION_X ) ) );
-		SIGMA_GL_DETECTION_Y = Float.parseFloat( props.getProperty( "SIGMA_GL_DETECTION_Y", Float.toString( SIGMA_GL_DETECTION_Y ) ) );
-		SEGMENTATION_MIX_CT_INTO_PMFRF = Float.parseFloat( props.getProperty( "SEGMENTATION_MIX_CT_INTO_PMFRF", Float.toString( SEGMENTATION_MIX_CT_INTO_PMFRF ) ) );
-		SEGMENTATION_MODEL_PATH = props.getProperty( "SEGMENTATION_MODEL_PATH", SEGMENTATION_MODEL_PATH);
-		DEFAULT_PATH = props.getProperty( "DEFAULT_PATH", DEFAULT_PATH );
-		CELL_LABELS = props.getProperty( "CELL_LABELS", CELL_LABELS);
-		CELL_LABEL_LIST = new ArrayList<>(Arrays.asList(CELL_LABELS.split(";")));
-
-		GUROBI_TIME_LIMIT = Double.parseDouble( props.getProperty( "GUROBI_TIME_LIMIT", Double.toString( GUROBI_TIME_LIMIT ) ) );
-		GUROBI_MAX_OPTIMALITY_GAP = Double.parseDouble( props.getProperty( "GUROBI_MAX_OPTIMALITY_GAP", Double.toString( GUROBI_MAX_OPTIMALITY_GAP ) ) );
-
-		/*
-		  Default x-position of the main GUI-window. This value will be used if the
-		  values in the properties file are not fitting on any of the currently
-		  attached screens.
-		 */
-		int DEFAULT_GUI_POS_X = 100;
-		GUI_POS_X = Integer.parseInt( props.getProperty( "GUI_POS_X", Integer.toString(DEFAULT_GUI_POS_X) ) );
-		GUI_POS_Y = Integer.parseInt( props.getProperty( "GUI_POS_Y", Integer.toString(DEFAULT_GUI_POS_X) ) );
-		GUI_WIDTH = Integer.parseInt( props.getProperty( "GUI_WIDTH", Integer.toString( GUI_WIDTH ) ) );
-		GUI_HEIGHT = Integer.parseInt( props.getProperty( "GUI_HEIGHT", Integer.toString( GUI_HEIGHT ) ) );
-		GUI_CONSOLE_WIDTH = Integer.parseInt( props.getProperty( "GUI_CONSOLE_WIDTH", Integer.toString( GUI_CONSOLE_WIDTH ) ) );
-		GUI_NUMBER_OF_SHOWN_TIMESTEPS = Integer.parseInt( props.getProperty( "GUI_NUMBER_OF_SHOWN_TIMESTEPS", Integer.toString(GUI_NUMBER_OF_SHOWN_TIMESTEPS) ) );
-
-		EXPORT_DO_TRACK_EXPORT = props.getProperty( "EXPORT_DO_TRACK_EXPORT", Integer.toString(EXPORT_DO_TRACK_EXPORT?1:0) ).equals("1");
-		EXPORT_USER_INPUTS = props.getProperty( "EXPORT_USER_INPUTS", Integer.toString(EXPORT_USER_INPUTS?1:0) ).equals("1");
-		EXPORT_INCLUDE_HISTOGRAMS = props.getProperty( "EXPORT_INCLUDE_HISTOGRAMS", Integer.toString(EXPORT_INCLUDE_HISTOGRAMS?1:0) ).equals("1");
-		EXPORT_INCLUDE_QUANTILES = props.getProperty( "EXPORT_INCLUDE_QUANTILES", Integer.toString(EXPORT_INCLUDE_QUANTILES?1:0) ).equals("1");
-		EXPORT_INCLUDE_COL_INTENSITY_SUMS = props.getProperty( "EXPORT_INCLUDE_COL_INTENSITY_SUMS", Integer.toString(EXPORT_INCLUDE_COL_INTENSITY_SUMS?1:0) ).equals("1");
-		EXPORT_INCLUDE_PIXEL_INTENSITIES = props.getProperty( "EXPORT_INCLUDE_PIXEL_INTENSITIES", Integer.toString(EXPORT_INCLUDE_PIXEL_INTENSITIES?1:0) ).equals("1");
-
-
-		OPTIMISATION_INTERVAL_LENGTH = Integer.parseInt( props.getProperty( "OPTIMISATION_INTERVAL_LENGTH", Integer.toString(OPTIMISATION_INTERVAL_LENGTH) ));
-
-		INTENSITY_FIT_ITERATIONS = Integer.parseInt(props.getProperty("INTENSITY_FIT_ITERATIONS", Integer.toString(INTENSITY_FIT_ITERATIONS)));
-		INTENSITY_FIT_PRECISION = Double.parseDouble(props.getProperty("INTENSITY_FIT_PRECISION", Double.toString(INTENSITY_FIT_PRECISION)));
-		INTENSITY_FIT_INITIAL_WIDTH = Double.parseDouble(props.getProperty("INTENSITY_FIT_INITIAL_WIDTH", Double.toString(INTENSITY_FIT_INITIAL_WIDTH)));
+		ConfigurationManager.load(optionalPropertyFile, currentPropertyFile, userMomaHomePropertyFile, momaUserDirectory);
+//		props = main.loadParams();
+//		BGREM_TEMPLATE_XMIN = Integer.parseInt( props.getProperty( "BGREM_TEMPLATE_XMIN", Integer.toString( BGREM_TEMPLATE_XMIN ) ) );
+//		BGREM_TEMPLATE_XMAX = Integer.parseInt( props.getProperty( "BGREM_TEMPLATE_XMAX", Integer.toString( BGREM_TEMPLATE_XMAX ) ) );
+//		BGREM_X_OFFSET = Integer.parseInt( props.getProperty( "BGREM_X_OFFSET", Integer.toString( BGREM_X_OFFSET ) ) );
+//		GL_WIDTH_IN_PIXELS = Integer.parseInt( props.getProperty( "GL_WIDTH_IN_PIXELS", Integer.toString( GL_WIDTH_IN_PIXELS ) ) );
+//		MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS = Integer.parseInt( props.getProperty( "MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS", Integer.toString( MOTHER_CELL_BOTTOM_TRICK_MAX_PIXELS ) ) );
+//		INTENSITY_FIT_RANGE_IN_PIXELS = Integer.parseInt( props.getProperty( "INTENSITY_FIT_RANGE_IN_PIXELS", Integer.toString( INTENSITY_FIT_RANGE_IN_PIXELS ) ) );
+//		GL_OFFSET_TOP = Integer.parseInt( props.getProperty( "GL_OFFSET_TOP", Integer.toString( GL_OFFSET_TOP ) ) );
+//		GL_OFFSET_LATERAL = Integer.parseInt( props.getProperty( "GL_OFFSET_LATERAL", Integer.toString( GL_OFFSET_LATERAL ) ) );
+//		MIN_CELL_LENGTH = Integer.parseInt( props.getProperty( "MIN_CELL_LENGTH", Integer.toString( MIN_CELL_LENGTH ) ) );
+//		MIN_GAP_CONTRAST = Float.parseFloat( props.getProperty( "MIN_GAP_CONTRAST", Float.toString( MIN_GAP_CONTRAST ) ) );
+//		SIGMA_PRE_SEGMENTATION_X = Float.parseFloat( props.getProperty( "SIGMA_PRE_SEGMENTATION_X", Float.toString( SIGMA_PRE_SEGMENTATION_X ) ) );
+//		SIGMA_PRE_SEGMENTATION_Y = Float.parseFloat( props.getProperty( "SIGMA_PRE_SEGMENTATION_Y", Float.toString( SIGMA_PRE_SEGMENTATION_Y ) ) );
+//		SIGMA_GL_DETECTION_X = Float.parseFloat( props.getProperty( "SIGMA_GL_DETECTION_X", Float.toString( SIGMA_GL_DETECTION_X ) ) );
+//		SIGMA_GL_DETECTION_Y = Float.parseFloat( props.getProperty( "SIGMA_GL_DETECTION_Y", Float.toString( SIGMA_GL_DETECTION_Y ) ) );
+//		SEGMENTATION_MIX_CT_INTO_PMFRF = Float.parseFloat( props.getProperty( "SEGMENTATION_MIX_CT_INTO_PMFRF", Float.toString( SEGMENTATION_MIX_CT_INTO_PMFRF ) ) );
+//		SEGMENTATION_MODEL_PATH = props.getProperty( "SEGMENTATION_MODEL_PATH", SEGMENTATION_MODEL_PATH);
+//		DEFAULT_PATH = props.getProperty( "DEFAULT_PATH", DEFAULT_PATH );
+//		CELL_LABELS = props.getProperty( "CELL_LABELS", CELL_LABELS);
+//		CELL_LABEL_LIST = new ArrayList<>(Arrays.asList(CELL_LABELS.split(";")));
+//
+//		GUROBI_TIME_LIMIT = Double.parseDouble( props.getProperty( "GUROBI_TIME_LIMIT", Double.toString( GUROBI_TIME_LIMIT ) ) );
+//		GUROBI_MAX_OPTIMALITY_GAP = Double.parseDouble( props.getProperty( "GUROBI_MAX_OPTIMALITY_GAP", Double.toString( GUROBI_MAX_OPTIMALITY_GAP ) ) );
+//
+//		/*
+//		  Default x-position of the main GUI-window. This value will be used if the
+//		  values in the properties file are not fitting on any of the currently
+//		  attached screens.
+//		 */
+//		int DEFAULT_GUI_POS_X = 100;
+//		GUI_POS_X = Integer.parseInt( props.getProperty( "GUI_POS_X", Integer.toString(DEFAULT_GUI_POS_X) ) );
+//		GUI_POS_Y = Integer.parseInt( props.getProperty( "GUI_POS_Y", Integer.toString(DEFAULT_GUI_POS_X) ) );
+//		GUI_WIDTH = Integer.parseInt( props.getProperty( "GUI_WIDTH", Integer.toString( GUI_WIDTH ) ) );
+//		GUI_HEIGHT = Integer.parseInt( props.getProperty( "GUI_HEIGHT", Integer.toString( GUI_HEIGHT ) ) );
+//		GUI_CONSOLE_WIDTH = Integer.parseInt( props.getProperty( "GUI_CONSOLE_WIDTH", Integer.toString( GUI_CONSOLE_WIDTH ) ) );
+//		GUI_NUMBER_OF_SHOWN_TIMESTEPS = Integer.parseInt( props.getProperty( "GUI_NUMBER_OF_SHOWN_TIMESTEPS", Integer.toString(GUI_NUMBER_OF_SHOWN_TIMESTEPS) ) );
+//
+//		EXPORT_DO_TRACK_EXPORT = props.getProperty( "EXPORT_DO_TRACK_EXPORT", Integer.toString(EXPORT_DO_TRACK_EXPORT?1:0) ).equals("1");
+//		EXPORT_USER_INPUTS = props.getProperty( "EXPORT_USER_INPUTS", Integer.toString(EXPORT_USER_INPUTS?1:0) ).equals("1");
+//		EXPORT_INCLUDE_HISTOGRAMS = props.getProperty( "EXPORT_INCLUDE_HISTOGRAMS", Integer.toString(EXPORT_INCLUDE_HISTOGRAMS?1:0) ).equals("1");
+//		EXPORT_INCLUDE_QUANTILES = props.getProperty( "EXPORT_INCLUDE_QUANTILES", Integer.toString(EXPORT_INCLUDE_QUANTILES?1:0) ).equals("1");
+//		EXPORT_INCLUDE_COL_INTENSITY_SUMS = props.getProperty( "EXPORT_INCLUDE_COL_INTENSITY_SUMS", Integer.toString(EXPORT_INCLUDE_COL_INTENSITY_SUMS?1:0) ).equals("1");
+//		EXPORT_INCLUDE_PIXEL_INTENSITIES = props.getProperty( "EXPORT_INCLUDE_PIXEL_INTENSITIES", Integer.toString(EXPORT_INCLUDE_PIXEL_INTENSITIES?1:0) ).equals("1");
+//
+//
+//		OPTIMISATION_INTERVAL_LENGTH = Integer.parseInt( props.getProperty( "OPTIMISATION_INTERVAL_LENGTH", Integer.toString(OPTIMISATION_INTERVAL_LENGTH) ));
+//
+//		INTENSITY_FIT_ITERATIONS = Integer.parseInt(props.getProperty("INTENSITY_FIT_ITERATIONS", Integer.toString(INTENSITY_FIT_ITERATIONS)));
+//		INTENSITY_FIT_PRECISION = Double.parseDouble(props.getProperty("INTENSITY_FIT_PRECISION", Double.toString(INTENSITY_FIT_PRECISION)));
+//		INTENSITY_FIT_INITIAL_WIDTH = Double.parseDouble(props.getProperty("INTENSITY_FIT_INITIAL_WIDTH", Double.toString(INTENSITY_FIT_INITIAL_WIDTH)));
 
 		if ( !HEADLESS ) {
 			// Iterate over all currently attached monitors and check if sceen
@@ -1039,68 +1042,68 @@ public class MoMA implements IImageProvider {
 		return selectedFile;
 	}
 
-	/**
-	 * Loads the file 'mm.properties' and returns an instance of
-	 * {@link Properties} containing the key-value pairs found in that file.
-	 *
-	 * @return instance of {@link Properties} containing the key-value pairs
-	 *         found in that file.
-	 */
-	private Properties loadParams() {
-		if (optionalPropertyFile != null) {
-			if (optionalPropertyFile.exists() && optionalPropertyFile.isFile()) {
-				try {
-					props = loadParameters(optionalPropertyFile);
-					currentPropertyFile = optionalPropertyFile;
-					return props;
-				} catch (IOException e) {
-				}
-			} else {
-				System.out.println("ERROR: The optional config file path does not exist:" + optionalPropertyFile.getPath());
-			}
-		}
+//	/**
+//	 * Loads the file 'mm.properties' and returns an instance of
+//	 * {@link Properties} containing the key-value pairs found in that file.
+//	 *
+//	 * @return instance of {@link Properties} containing the key-value pairs
+//	 *         found in that file.
+//	 */
+//	private Properties loadParams() {
+//		if (optionalPropertyFile != null) {
+//			if (optionalPropertyFile.exists() && optionalPropertyFile.isFile()) {
+//				try {
+//					props = loadParameters(optionalPropertyFile);
+//					currentPropertyFile = optionalPropertyFile;
+//					return props;
+//				} catch (IOException e) {
+//				}
+//			} else {
+//				System.out.println("ERROR: The optional config file path does not exist:" + optionalPropertyFile.getPath());
+//			}
+//		}
+//
+//		if(!userMomaHomePropertyFile.exists())
+//		{
+//			if(!momaUserDirectory.exists())
+//			{
+//				momaUserDirectory.mkdir();
+//			}
+//			// HERE WE NEED TO SETUP THE HOME-DIRECTORY SETTINGS, IF THEY DO NOT EXIST
+//			final File f = new File( "mm.properties" );
+//			if(f.isFile()){
+//				try {
+//					FileUtils.copyFile(f, userMomaHomePropertyFile);
+//				} catch (IOException e) {
+//					System.out.println( "Failed to copy user file to default property file to user moma directory: " +  momaUserDirectory.getPath());
+//					e.printStackTrace();
+//				}
+//			}
+//			System.out.println( "Performed setup of new default property file here: " + userMomaHomePropertyFile.getAbsolutePath() );
+//		}
+//
+//		try {
+//			currentPropertyFile = userMomaHomePropertyFile;
+//			return loadParameters(userMomaHomePropertyFile);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("ERROR: Failed to load property file.");
+//		return props;
+//	}
 
-		if(!userMomaHomePropertyFile.exists())
-		{
-			if(!momaUserDirectory.exists())
-			{
-				momaUserDirectory.mkdir();
-			}
-			// HERE WE NEED TO SETUP THE HOME-DIRECTORY SETTINGS, IF THEY DO NOT EXIST
-			final File f = new File( "mm.properties" );
-			if(f.isFile()){
-				try {
-					FileUtils.copyFile(f, userMomaHomePropertyFile);
-				} catch (IOException e) {
-					System.out.println( "Failed to copy user file to default property file to user moma directory: " +  momaUserDirectory.getPath());
-					e.printStackTrace();
-				}
-			}
-			System.out.println( "Performed setup of new default property file here: " + userMomaHomePropertyFile.getAbsolutePath() );
-		}
-
-		try {
-			currentPropertyFile = userMomaHomePropertyFile;
-			return loadParameters(userMomaHomePropertyFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("ERROR: Failed to load property file.");
-		return props;
-	}
-
-	/**
-	 * Load parameters from the provided config-file.
-	 * @param configFile
-	 * @return
-	 * @throws IOException
-	 */
-	private Properties loadParameters(File configFile) throws IOException {
-		FileInputStream is = new FileInputStream(configFile);
-		final Properties props = new Properties();
-		props.load( is );
-		return props;
-	}
+//	/**
+//	 * Load parameters from the provided config-file.
+//	 * @param configFile
+//	 * @return
+//	 * @throws IOException
+//	 */
+//	private Properties loadParameters(File configFile) throws IOException {
+//		FileInputStream is = new FileInputStream(configFile);
+//		final Properties props = new Properties();
+//		props.load( is );
+//		return props;
+//	}
 
 	/**
 	 * Save parameters to the currently used config file.

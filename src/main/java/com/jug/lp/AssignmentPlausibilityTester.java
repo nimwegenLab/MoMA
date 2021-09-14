fceff381f108d9cb8004c3a6172b5491061bbbcd
Michael Mell <michael.mell@unibas.ch>
2021-09-14 14:42:03 +0200

    Add class for future plausibility tests.

---
+++ src/main/java/com/jug/lp/AssignmentPlausibilityTester.java
@@ -0,0 +1,34 @@
+package com.jug.lp;
+
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * Methods in this class are used to test whether the combination of components in assignments are physically plausible.
+ * They are used to determine whether an assignment should be added to the optimizer.
+ */
+public class AssignmentPlausibilityTester {
+
+    double growthRateInFrames = 0;
+
+    private double sizeDifferenceThreshold;
+
+    /**
+     * Test if the size difference between the source component and the total size of the target components is plausible.
+     * @param sourceComponentSize
+     * @param totalTargetComponentSize
+     * @return
+     */
+    public boolean sizeDifferenceIsPlausible(int sourceComponentSize, int totalTargetComponentSize) {
+        double foldChange = ((double) totalTargetComponentSize) / ((double) sourceComponentSize);
+        return foldChange <= sizeDifferenceThreshold;
+    }
+
+    public void setShortestDoublingTimeInFrames(int shortestDoublingTimeInFrames) {
+        this.growthRateInFrames = Math.log(2) / ((double) shortestDoublingTimeInFrames);
+        this.sizeDifferenceThreshold = Math.exp(growthRateInFrames * 1); /* *1 is symbolic and represent the single frame time step for which we calculate the change; i.e.: N_t/N_0 = exp(growthRate*t) */
+    }
+
+    public double getGrowthRateInFrames() {
+        return growthRateInFrames;
+    }
+}
---
+++ src/test/java/com/jug/lp/AssignmentPlausibilityTesterTest.java
@@ -0,0 +1,56 @@
+package com.jug.lp;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AssignmentPlausibilityTesterTest {
+
+    @Test
+    public void sizeDifferenceIsPlausible__for_smaller_than_valid_target_size__returns_true() {
+        int sourceSize = 10;
+        int totalTargetSize = 19;
+        int doublingTime = 1;
+        AssignmentPlausibilityTester sut = new AssignmentPlausibilityTester();
+        sut.setShortestDoublingTimeInFrames(doublingTime);
+        assertTrue(sut.sizeDifferenceIsPlausible(sourceSize, totalTargetSize));
+    }
+
+    @Test
+    public void sizeDifferenceIsPlausible__for_equal_to_valid_target_size__returns_true() {
+        int sourceSize = 10;
+        int totalTargetSize = 20;
+        int doublingTime = 1;
+        AssignmentPlausibilityTester sut = new AssignmentPlausibilityTester();
+        sut.setShortestDoublingTimeInFrames(doublingTime);
+        assertTrue(sut.sizeDifferenceIsPlausible(sourceSize, totalTargetSize));
+    }
+
+    @Test
+    public void sizeDifferenceIsPlausible__for_valid_target_size__returns_false() {
+        int sourceSize = 10;
+        int totalTargetSize = 21;
+        int doublingTime = 1;
+        AssignmentPlausibilityTester sut = new AssignmentPlausibilityTester();
+        sut.setShortestDoublingTimeInFrames(doublingTime);
+        assertFalse(sut.sizeDifferenceIsPlausible(sourceSize, totalTargetSize));
+    }
+
+    @Test
+    public void getShortestDoublingTimeInFrames__returns_expected_value() {
+        double expected = 0.0346;
+        int doublingTime = 20;
+        AssignmentPlausibilityTester sut = new AssignmentPlausibilityTester();
+        sut.setShortestDoublingTimeInFrames(doublingTime);
+        assertEquals(expected, sut.getGrowthRateInFrames(), 1e-3);
+    }
+
+    @Test
+    public void setShortestDoublingTimeInFrames__sets_correct_growth_rate() {
+        double expected = 0.0346;
+        int doublingTime = 20;
+        AssignmentPlausibilityTester sut = new AssignmentPlausibilityTester();
+        sut.setShortestDoublingTimeInFrames(doublingTime);
+        assertEquals(expected, sut.getGrowthRateInFrames(), 1e-3);
+    }
+}

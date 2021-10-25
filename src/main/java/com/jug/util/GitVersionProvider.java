f641932983cbd18364857d97130c3ac37ef9c708
Michael Mell <michael.mell@unibas.ch>
2021-10-25 13:42:49 +0200

    Got Jackson to work for parsing the JSON file.

--- src/main/java/com/jug/util/GitVersionProvider.java
+++ src/main/java/com/jug/util/GitVersionProvider.java
@@ -1,5 +1,10 @@
 package com.jug.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang.NotImplementedException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,16 +14,34 @@ public class GitVersionProvider {
     public GitVersionProvider() {
     }
 
-    public String getVersionString(){
-        return readGitVersion();
+    public String getVersionString() {
+        String versionString;
+        try {
+            versionString = getGitVersionInfo();
+        } catch (IOException e) {
+            return "Error: Json Git Version could not be parsed";
+        }
+        return versionString;
+//        throw new NotImplementedException();
+//        return readGitVersion();
+    }
+
+    private String getGitVersionInfo() throws IOException {
+        String jsonString = readGitVersion();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonString);
+        String version = rootNode.get("git.closest.tag.name").asText();
+        version = version.substring(1);
+        String commitId = rootNode.get("git.commit.id").asText().substring(0, 8);
+        return version + "-" + commitId;
     }
 
     private String readGitVersion() {
         ClassLoader classLoader = getClass().getClassLoader();
         InputStream intputStream = classLoader.getResourceAsStream("git.properties");
-        try{
+        try {
             return readFromInputStream(intputStream);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return "Version information could not be retrieved";
         }
@@ -28,7 +51,7 @@ public class GitVersionProvider {
         StringBuilder resultStringBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 resultStringBuilder.append(line).append("\n");
             }
         }

cbea6e9a6dc77260ad6743e5cb82946c0548e45b
Michael Mell <michael.mell@unibas.ch>
2021-11-01 09:29:13 +0100

    Modify test.

--- src/test/java/com/jug/MoMATest.java
+++ src/test/java/com/jug/MoMATest.java
@@ -69,8 +69,8 @@ public class MoMATest {
         String inputPath = datasets_base_path + "/000_moma_benchmarking/CIP/lis_20201119/Pos6_GL6/cropped__20201119_VNG1040_AB2h_2h_1_MMStack_Pos6_GL6.tif";
         String outputPath = datasets_base_path + "000_moma_benchmarking/CIP/lis_20201119/Pos6_GL6/output/";
         Integer tmin = 0;
-        Integer tmax = 10;
-        startMoma(true, inputPath, outputPath, tmin, tmax, true, new String[]{"-ground_truth_export"});
+        Integer tmax = 120;
+        startMoma(false, inputPath, outputPath, tmin, tmax, true, new String[]{"-ground_truth_export"});
     }
 
     @Test

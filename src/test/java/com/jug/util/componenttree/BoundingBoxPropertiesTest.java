d83062d2617c31890a282b9dca2f9c1e2f34ba14
Michael Mell <michael.mell@unibas.ch>
2021-12-22 21:35:18 +0100

    Rename tests. Add bbox test for horizontally tilted bounding box, where the length in x-direction is larger than in y-direction.

--- src/test/java/com/jug/util/componenttree/BoundingBoxPropertiesTest.java
+++ src/test/java/com/jug/util/componenttree/BoundingBoxPropertiesTest.java
@@ -8,13 +8,30 @@ import static org.junit.Assert.*;
 
 public class BoundingBoxPropertiesTest {
     @Test
-    public void test(){
+    public void test_returned_properties_for_vertically_aligned_bbox(){
         double expectedArea = 5000.0;
         double expectedHeight = 100.0;
         double expectedWidth = 50.0;
         Vector2D expectedCenterCoordinate = new Vector2D(25, 50);
         double[] xCoordsExpected = new double[]{0, 50, 50, 0};
         double[] yCoordsExpected = new double[]{0, 0, 100, 100};
+        Polygon2D orientedBoundingBoxPolygon = GeomMasks.polygon2D(xCoordsExpected, yCoordsExpected);
+        BoundingBoxProperties bboxProps = new BoundingBoxProperties(orientedBoundingBoxPolygon);
+        assertEquals(expectedArea, bboxProps.getArea(), 0.01);
+        assertEquals(expectedHeight, bboxProps.getHeight(), 0.01);
+        assertEquals(expectedWidth, bboxProps.getWidth(), 0.01);
+        assertEquals(expectedWidth, bboxProps.getWidth(), 0.01);
+        assertTrue("bbox center is incorrect", bboxProps.getCenterCoordinate().equals(expectedCenterCoordinate, 0.01));
+    }
+
+    @Test
+    public void test_returned_properties_for_horizontally_aligned_bbox(){
+        double expectedArea = 5000.0;
+        double expectedHeight = 100.0;
+        double expectedWidth = 50.0;
+        Vector2D expectedCenterCoordinate = new Vector2D(50,25);
+        double[] xCoordsExpected = new double[]{0, 100, 100, 0};
+        double[] yCoordsExpected = new double[]{0, 0, 50, 50};
         Polygon2D orientedBoundingBoxPolygon = GeomMasks.polygon2D(xCoordsExpected, yCoordsExpected);
         BoundingBoxProperties bboxProps = new BoundingBoxProperties(orientedBoundingBoxPolygon);
         assertEquals(expectedArea, bboxProps.getArea(), 0.01);

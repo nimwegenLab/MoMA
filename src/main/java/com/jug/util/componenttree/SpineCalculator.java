2d874c826447bfe5b7e4f8c7b59e594cdc18fc47
Michael Mell <michael.mell@unibas.ch>
2022-01-13 15:02:30 +0100

    Do not average, if the averaging size does not permit it.

--- src/main/java/com/jug/util/componenttree/SpineCalculator.java
+++ src/main/java/com/jug/util/componenttree/SpineCalculator.java
@@ -32,7 +32,7 @@ public class SpineCalculator {
         Vector2D basePoint1 = medialLine.get(0);
         if(Math.round(basePoint1.getY()) != imageLimitsYdirection.getA()){ /* this is catches the situation, where the medial line starts on the image-boundary; this happens for components that sit on the image-boundary */
             List<Vector2D> diffsAtStart = diffs.subList(0, orientationVectorAveragingWindowSize);
-            Vector2D orientationVector1 = GeomUtils.averageVectors(diffsAtStart).multiply(-1.0); /* multiply(-1.0): we invert direction because diffs will point towards the center of the medial line */
+            Vector2D orientationVector1 = diffsAtStart.size() > 1 ? GeomUtils.averageVectors(diffsAtStart).multiply(-1.0) : diffsAtStart.get(0);
             Vector2D result1 = calculateInterceptWithContour(linkedContour, orientationVector1, basePoint1);
             spine.add(0, result1);
         }
@@ -40,7 +40,7 @@ public class SpineCalculator {
         Vector2D basePoint2 = medialLine.get(medialLine.size() - 1);
         if(Math.round(basePoint2.getY()) != imageLimitsYdirection.getB()) { /* this is catches the situation, where the medial line starts on the image-boundary; this happens for components that sit on the image-boundary */
             List<Vector2D> diffsAtEnd = diffs.subList(diffs.size() - 1 - orientationVectorAveragingWindowSize, diffs.size() - 1);
-            Vector2D orientationVector2 = GeomUtils.averageVectors(diffsAtEnd);
+            Vector2D orientationVector2 = diffsAtEnd.size() > 1 ? GeomUtils.averageVectors(diffsAtEnd) : diffsAtEnd.get(diffsAtEnd.size() - 1);
             Vector2D result2 = calculateInterceptWithContour(linkedContour, orientationVector2, basePoint2);
             spine.add(result2);
         }

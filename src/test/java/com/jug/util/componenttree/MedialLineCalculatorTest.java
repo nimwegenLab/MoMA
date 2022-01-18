91e914eae44897ab76715e1d857a8126c403cb5e
Michael Mell <michael.mell@unibas.ch>
2022-01-18 12:16:30 +0100

    Add test-duplicate with assertions.

--- src/test/java/com/jug/util/componenttree/MedialLineCalculatorTest.java
+++ src/test/java/com/jug/util/componenttree/MedialLineCalculatorTest.java
@@ -17,6 +17,8 @@ import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.ValuePair;
 import net.imglib2.view.Views;
+import org.junit.Test;
+import org.junit.Assert;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +28,7 @@ import java.util.List;
 public class MedialLineCalculatorTest {
     private final ImageJ ij;
     private final TestUtils testUtils;
+    private double delta = 1e-5;
 
     public MedialLineCalculatorTest() {
         ij = new ImageJ();
@@ -106,6 +109,56 @@ public class MedialLineCalculatorTest {
                 spine.getPolyline()
         );
         testUtils.showImageWithOverlays(image, rois);
+    }
+
+    /**
+     * Add test for calculating the medial line.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    public void exploreSpine3Calculator() throws IOException {
+        String imageFile = new File("").getAbsolutePath() + "/src/test/resources/00_probability_maps/20201119_VNG1040_AB2h_2h_1_MMStack_Pos6_GL6/frame90_repeated__cropped__20201119_VNG1040_AB2h_2h_1_MMStack_Pos6_GL6__model_9e5727e4ed18802f4ab04c7494ef8992d798f4d64d5fd75e285b9a3d83b13ac9.tif";
+
+        int componentIndex = 2;
+
+        ValuePair<AdvancedComponent<FloatType>, RandomAccessibleInterval<BitType>> componentAndImage = testUtils.getComponentWithImage(imageFile,
+                componentIndex,
+                new BitType(true));
+        AdvancedComponent<FloatType> component = componentAndImage.getA();
+        RandomAccessibleInterval<BitType> image = component.getComponentImage(new BitType(true));
+
+        MedialLineCalculator medialLineCalculator = new MedialLineCalculator(ij.op(), new Imglib2Utils(ij.op()));
+        Vector2DPolyline medialLine = medialLineCalculator.calculate(image);
+        ContourCalculator contourCalculator = new ContourCalculator(ij.op());
+
+        LabelRegion<Integer> componentRegion = componentAndImage.getA().getRegion();
+        Vector2DPolyline contour = contourCalculator.calculate(componentRegion);
+
+        SpineCalculator sut = new SpineCalculator(21, 21, 3.5);
+
+        Vector2DPolyline spine = sut.calculate(medialLine, contour, new ValuePair<>((int) image.min(1), (int) image.max(1)));
+
+        contour.shiftMutate(new Vector2D(0.5, 0.5));
+        medialLine.shiftMutate(new Vector2D(0.5, 0.5));
+        spine.shiftMutate(new Vector2D(0.5, 0.5));
+
+        List<MaskPredicate<?>> rois = Arrays.asList(
+                contour.getPolygon2D(),
+                medialLine.getPolyline(),
+                spine.getPolyline()
+        );
+
+        Vector2D first = spine.getFirst();
+        int centerInd = spine.size() / 2;
+        Vector2D centerPoint = spine.get(centerInd);
+        Vector2D last = spine.getLast();
+        TestUtils.assertEqual(new Vector2D(51.73809523809524, 95.5), first, delta);
+        TestUtils.assertEqual(new Vector2D(54.785714285714285, 162.5), centerPoint, delta);
+        TestUtils.assertEqual(new Vector2D(52.46888888888889, 232.5), last, delta);
+
+//        testUtils.showImageWithOverlays(image, rois);
     }
 
     /**

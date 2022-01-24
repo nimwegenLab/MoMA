8b12abf00949f41551496461c003edb57a646017
Michael Mell <michael.mell@unibas.ch>
2022-01-24 16:04:44 +0100

    Test the names of the retrieved variables. The asserts work.

--- src/test/java/com/jug/lp/TestGurobiVariableSettingAndRetrieval.java
+++ src/test/java/com/jug/lp/TestGurobiVariableSettingAndRetrieval.java
@@ -41,37 +41,43 @@ public class TestGurobiVariableSettingAndRetrieval {
 
     @Test
     public void testSaveAndLoadVariable_1() throws IOException, GRBException {
-        Path path = Files.createTempDirectory("java-");
+        Path pathToTempDir = Files.createTempDirectory("java-");
 
         /* create and save model */
-        String modelFilePath = path.toAbsolutePath() + "/model.lp";
-        GRBEnv env = new GRBEnv(path.toAbsolutePath() + "/ilp_orig.log");
+        System.out.println("Temporary Directory: " + pathToTempDir.toAbsolutePath());
+        String modelFilePath = pathToTempDir.toAbsolutePath() + "/model.lp";
+        GRBEnv env = new GRBEnv(pathToTempDir.toAbsolutePath() + "/ilp_orig.log");
 //        env.start();
         GRBModel model = new GRBModel(env);
         String name = "some_variable_name";
         String var1name = name + "1";
         String var2name = name + "2";
-        GRBVar var1 = model.addVar(0.0, 1.0, 1.0, GRB.BINARY, var1name);
-        GRBVar var2 = model.addVar(0.0, 1.0, 1.0, GRB.BINARY, var2name);
+        model.addVar(0.0, 1.0, 1.0, GRB.BINARY, var1name);
+        model.addVar(0.0, 1.0, 1.0, GRB.BINARY, var2name);
         model.update();
         model.write(modelFilePath);
 
         /* load model and retrieve variables */
-        GRBEnv envLoaded = new GRBEnv(path.toAbsolutePath() + "/ilp_loaded.log");
+        GRBEnv envLoaded = new GRBEnv(pathToTempDir.toAbsolutePath() + "/ilp_loaded.log");
 //        env.start();
         GRBModel modelLoaded = new GRBModel(envLoaded, modelFilePath);
 //        GRBVar[] vars = model.getVars();
 //        System.out.println("vars.length: " + vars.length);
 //        GRBVar res = vars[0];
-        GRBVar var1_retrieved = modelLoaded.getVarByName(var1name);
+//        GRBVar var1_retrieved = modelLoaded.getVarByName(var1name);
 //        var1_retrieved.get("name")
 //        Assert.assertTrue(var1.sameAs(var1_retrieved));
 
+        GRBVar var1_retrieved = modelLoaded.getVarByName(var1name);
+        String var1name_retrieved = var1_retrieved.get(GRB.StringAttr.VarName);
+        Assert.assertTrue(var1name.contentEquals(var1name_retrieved));
+
         GRBVar var2_retrieved = modelLoaded.getVarByName(var2name);
-//        Assert.assertTrue(var2.sameAs(var2_retrieved));
+        String var2name_retrieved = var2_retrieved.get(GRB.StringAttr.VarName);
+        Assert.assertTrue(var2name.contentEquals(var2name_retrieved));
 
 //        Assert.assertFalse(var1.sameAs(var2_retrieved));
-        System.out.println("stop");
+//        System.out.println("stop");
 //        String.format("a_%d^LYSIS--%d", t, hyp.getId())
 
     }

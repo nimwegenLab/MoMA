36b6f255c28889a5beb4f3a43233f860e29fa712
Michael Mell <michael.mell@unibas.ch>
2022-03-01 17:35:59 +0100

    Add exploration test for outputting pixel moments for analysis.

--- src/test/java/com/jug/exploration/ExploreComponentMomentsExport.java
+++ src/test/java/com/jug/exploration/ExploreComponentMomentsExport.java
@@ -10,18 +10,19 @@ import java.nio.file.Paths;
 import static com.jug.util.JavaUtils.concatenateWithCollection;
 
 public class ExploreComponentMomentsExport {
-    String datasets_base_path = "/media/micha/T7/20210816_test_data_michael/Moma/MM_Testing/";
+    String datasets_base_path = "/media/micha/T7/20210816_test_data_michael/Moma/MM_Testing/000_development/feature/20220216-export-pixel-moments/";
 
     public static void main(String[] args) {
         ExploreComponentMomentsExport tests = new ExploreComponentMomentsExport();
-        tests._dany_20200812_8proms_ace_1_MMStack_Pos25_GL22();
+        tests._dany_20200812_8proms_ace_1_MMStack_Pos25_GL7();
     }
 
-    public void _dany_20200812_8proms_ace_1_MMStack_Pos25_GL22() {
-        String inputPath = datasets_base_path + "/001_moma_debugging/20200812_8proms_ace_1/Pos25_GL22/20200812_8proms_ace_1_MMStack_Pos25_GL22.tif";
-        String outputPath = datasets_base_path + "/001_moma_debugging/20200812_8proms_ace_1/Pos25_GL22/output/";
+    public void _dany_20200812_8proms_ace_1_MMStack_Pos25_GL7() {
+        String datasetSubPath = "/dany_20200812_8proms_ace_1_MMStack/Pos25_GL7/";
+        String inputPath = datasets_base_path + datasetSubPath + "/20200812_8proms_ace_1_MMStack_Pos25_GL7.tif";
+        String outputPath = datasets_base_path + datasetSubPath + "/output/";
         Integer tmin = 0;
-        Integer tmax = 100;
+        Integer tmax = null;
         startMoma(false, inputPath, outputPath, tmin, tmax, true, new String[]{"-ground_truth_export"});
     }
 

926fcde0d266d7b996db85dfdfa45959c49eb399
Michael Mell <michael.mell@unibas.ch>
2022-04-05 12:40:54 +0200

    Fix test.

--- src/test/java/com/jug/export/SpineLengthMeasurementTest.java
+++ src/test/java/com/jug/export/SpineLengthMeasurementTest.java
@@ -51,14 +51,14 @@ public class SpineLengthMeasurementTest {
             SegmentMeasurementData data = new SegmentMeasurementData(component, new ArrayList<>());
             sut.measure(data);
         }
-        ResultTableColumn<Double> column = resultTable.columnList.get(0);
+        ResultTableColumn<Double> column = resultTable.columnList.get(1);
 
         double[] expected = new double[]{
-                42.65685424949238D,
-                42.65685424949238D,
-                42.65685424949238D,
-                42.65685424949238D,
-                42.65685424949238D,
+                41.13496287766395D,
+                47.08039933241312D,
+                137.37517972316357D,
+                45.412324125174486D,
+                79.65491798744475D,
         };
         for (int componentIndex = 0; componentIndex < 5; componentIndex++) {
             System.out.println("componentIndex: " + componentIndex);

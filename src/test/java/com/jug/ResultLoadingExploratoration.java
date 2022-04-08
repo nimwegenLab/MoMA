93745b60673882883c4091f9ec8eadcab0a15faf
Michael Mell <michael.mell@unibas.ch>
2022-04-08 13:09:54 +0200

    Change path to renamed file structured.

--- src/test/java/com/jug/ResultLoadingExploratoration.java
+++ src/test/java/com/jug/ResultLoadingExploratoration.java
@@ -10,7 +10,7 @@ import static com.jug.util.JavaUtils.concatenateWithCollection;
 public class ResultLoadingExploratoration {
     //    String datasets_base_path = "/home/micha/Documents/01_work/git/MoMA/test_datasets";
 //    String datasets_base_path = "/media/micha/T7/20210816_test_data_michael/home__micha__Documents__01_work/MoMA/test_datasets";
-    String datasets_base_path = "/media/micha/T7/20210816_test_data_michael/Moma/MM_Testing/000_development/feature/20220121-fix-loading-of-curated-datasets/";
+    String datasets_base_path = "/media/micha/T7/data_michael_mell/moma_test_data/000_development/feature/20220121-fix-loading-of-curated-datasets/";
 
 //-i
 ///home/micha/Documents/01_work/git/MoMA/test_datasets/Dany_synthetic_rich_media/cropped_420200403_5_rpmB_1_rrnB_synthrich7_1_MMStack_Pos13_preproc_GL16.tif
@@ -20,8 +20,8 @@ public class ResultLoadingExploratoration {
 
     public static void main(String[] args) {
         ResultLoadingExploratoration tests = new ResultLoadingExploratoration();
-//        tests._dany_20200730_4proms_glu_ez1x_1_MMStack_Pos3_GL16();
-        tests._20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12();
+        tests._dany_20200730_4proms_glu_ez1x_1_MMStack_Pos3_GL16();
+//        tests._20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12();
     }
     public void _dany_20200730_4proms_glu_ez1x_1_MMStack_Pos3_GL16() {
         String inputPath = datasets_base_path + "/dany_20200730__Pos3_GL16/20200730_4proms_glu_ez1x_1_MMStack_Pos3_GL16.tif";

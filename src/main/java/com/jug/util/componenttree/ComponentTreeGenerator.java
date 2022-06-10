4e2e7430c7e08fcd2bfc46aa1347d7ee05a7758a
Michael Mell <michael.mell@unibas.ch>
2022-06-10 12:10:52 +0200

    Refactor ComponentTreeGenerator.buildIntensityTree to take RandomAccessibleInterval as first argument to make testing easier.

--- src/main/java/com/jug/util/componenttree/ComponentTreeGenerator.java
+++ src/main/java/com/jug/util/componenttree/ComponentTreeGenerator.java
@@ -37,7 +37,6 @@ public class ComponentTreeGenerator {
     }
 
     public ComponentForest<AdvancedComponent<FloatType>> buildIntensityTree(Img<FloatType> raiFkt, int frameIndex, float componentSplittingThreshold) {
-
         /* generate image mask for component generation; watershedMaskGenerator.generateMask(...) also merges adjacent connected components, if values between do fall below a given cutoff (see implementation) */
         Img<BitType> mask = watershedMaskGenerator.generateMask(ImgView.wrap(raiFkt));
 

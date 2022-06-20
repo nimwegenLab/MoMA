96869e8e0467e821c4333398f475fd2cf4ce4b51
Michael Mell <michael.mell@unibas.ch>
2022-06-20 20:36:32 +0200

    WIP: Start work on persisting optimization range to mm.properties
    * Remove optrange argument from test.

--- src/test/java/com/jug/exploration/ExploreResultLoading.java
+++ src/test/java/com/jug/exploration/ExploreResultLoading.java
@@ -55,7 +55,7 @@ public class ExploreResultLoading {
         Path properties_file_path = Paths.get(datasets_base_path, subfolder, "mm.properties");
         Integer tmin = 1;
         Integer tmax = 120;
-        startMoma(false, inputPath.toString(), outputPath.toString(), tmin, tmax, false, new String[]{"-ground_truth_export", "-p", properties_file_path.toString(), "-optrange", "10"});
+        startMoma(false, inputPath.toString(), outputPath.toString(), tmin, tmax, false, new String[]{"-ground_truth_export", "-p", properties_file_path.toString()});
 //        startMoma(false, null, null, null, null, false, new String[]{"-ground_truth_export", "-reload", reload_folder_path});
     }
 

4a19c382db023b0bbac1f1c89d8674b2770a3f42
Michael Mell <michael.mell@unibas.ch>
2022-08-14 17:29:32 +0200

    Add test-case with cell-lysis.

--- src/test/java/com/jug/exploration/ExplorationForImprovingCostFunctions.java
+++ src/test/java/com/jug/exploration/ExplorationForImprovingCostFunctions.java
@@ -12,13 +12,27 @@ public class ExplorationForImprovingCostFunctions {
     public static void main(String[] args) {
         ExplorationForImprovingCostFunctions tests = new ExplorationForImprovingCostFunctions();
 
-        tests._20220812_test_using_cell_area_below_component_for_migration_cost();
+        tests._20220812_test_using_cell_area_below_component_for_migration_cost___lis__20211026__Pos7_GL12();
+//        tests._20220812_test_using_cell_area_below_component_for_migration_cost___lis__20220701__Pos1_GL2();
 //        tests._lis_20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12();
 //        tests._dany_20200812_8proms_ace_1_MMStack_Pos25_GL7();
     }
 
-    public void _20220812_test_using_cell_area_below_component_for_migration_cost() {
-        String datasetSubPath = "20220812-test-using-cell-area-below-component-for-migration-cost";
+    public void _20220812_test_using_cell_area_below_component_for_migration_cost___lis__20211026__Pos7_GL12() {
+        String datasetSubPath = "20220812-test-using-cell-area-below-component-for-migration-cost/lis__20211026__Pos7_GL12";
+        Path inputPath = Paths.get(datasets_base_path, datasetSubPath, "Pos7_GL12", "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+        Path outputPath = Paths.get(datasets_base_path, datasetSubPath, "output");
+        if (!outputPath.toFile().exists()) {
+            outputPath.toFile().mkdir();
+        }
+        Path mmPropertiesPath = Paths.get(datasets_base_path, datasetSubPath, "mm.properties");
+        Integer tmin = 400;
+        Integer tmax = 450;
+        startMoma(false, inputPath.toString(), outputPath.toString(), tmin, tmax, true, new String[]{"-p",mmPropertiesPath.toString()});
+    }
+
+    public void _20220812_test_using_cell_area_below_component_for_migration_cost___lis__20220701__Pos1_GL2() {
+        String datasetSubPath = "20220812-test-using-cell-area-below-component-for-migration-cost/lis__20220701__Pos1_GL2";
         Path inputPath = Paths.get(datasets_base_path, datasetSubPath, "Pos1_GL2", "20220701_VNG1040_AB2h_4_MMStack_Pos0_Pos1_GL2.tif");
         Path outputPath = Paths.get(datasets_base_path, datasetSubPath, "output");
         if (!outputPath.toFile().exists()) {

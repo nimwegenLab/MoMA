dea1411c2c79248786aa0df477c94f088f278af6
Michael Mell <michael.mell@unibas.ch>
2022-08-15 12:37:23 +0200

    Ignore version-check, when debugging.

--- src/main/java/com/jug/datahandling/VersionCompatibilityChecker.java
+++ src/main/java/com/jug/datahandling/VersionCompatibilityChecker.java
@@ -2,10 +2,13 @@ package com.jug.datahandling;
 
 public class VersionCompatibilityChecker {
     public boolean versionAreCompatible(String momaVersionOfRunningInstance, String momaVersionOfDataset) {
+        if (momaVersionOfRunningInstance.contains("git_version_could_not_be_read") || momaVersionOfDataset.contains("git_version_could_not_be_read")) {
+            return true; /* we are running in the IDE */
+        }
         return momaVersionOfDataset.equals(momaVersionOfRunningInstance);
     }
 
-    public String getErrorMessage(String momaVersionOfRunningInstance, String momaVersionOfDataset){
+    public String getErrorMessage(String momaVersionOfRunningInstance, String momaVersionOfDataset) {
         return "\tDataset version is incompatible:\n\tDataset version: " + momaVersionOfDataset + "\n\tMoMA version: " + momaVersionOfRunningInstance;
     }
 }

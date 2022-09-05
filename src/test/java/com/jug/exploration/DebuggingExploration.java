4b9993ce9567ff129626e9184729dda616589485
Michael Mell <michael.mell@unibas.ch>
2022-09-05 14:22:51 +0200

    Reproduce error.

--- src/test/java/com/jug/exploration/DebuggingExploration.java
+++ src/test/java/com/jug/exploration/DebuggingExploration.java
@@ -12,6 +12,9 @@ public class DebuggingExploration {
     public static void main(String[] args) {
         DebuggingExploration tests = new DebuggingExploration();
 
+        tests._20220905_fix_incorrect_export_folder_path__test_exporting();
+//        tests._20220905_fix_incorrect_export_folder_path__test_trackonly();
+
 //        tests._20220817_debug_missing_assignments_and_components__test_4__Pos27_GL16();
 //        tests._20220817_debug_missing_assignments_and_components__test_3__Pos17_GL31();
 //        tests._20220817_debug_missing_assignments_and_components__test_2__Pos0_GL7();
@@ -20,7 +23,7 @@ public class DebuggingExploration {
 //        tests._20220816_fix_opt_range_slider();
 //        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_interactive_tracking();
 //        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_interactive_tracking_reload();
-        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_trackonly();
+//        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_trackonly();
 //        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_reloading();
 //        tests._20220815_fix_moma_fails_does_not_correctly_restore_ilp_state__test_reloading_scicore_tracking_result();
 //        tests._20220811_fix_issue_with_continuity_constraint_violation();
@@ -28,6 +31,23 @@ public class DebuggingExploration {
 //        tests._20220525_endoftracking_terminator_not_being_written_to_cell_stats_csv_file_1();
 //        tests._2020524_fix_issue_with_non_exported_cell_mask__reproduce_issue();
 //        tests._2020524_fix_issue_with_non_exported_cell_mask__debug_issue();
+    }
+
+    public void _20220905_fix_incorrect_export_folder_path__test_exporting() {
+        String datasetSubPath = "20220815-fix-moma-fails-does-not-correctly-restore-ilp-state";
+        Path glPath = Paths.get(datasets_base_path, datasetSubPath, "Pos0_GL7");
+        String analysisName = "debug_analysis__20220905_fix_incorrect_export_folder_path";
+        startMoma(true, null, null, null, null, false, new String[]{"-reload",glPath.toString(),"-analysis",analysisName});
+    }
+
+    public void _20220905_fix_incorrect_export_folder_path__test_trackonly() {
+        String datasetSubPath = "20220815-fix-moma-fails-does-not-correctly-restore-ilp-state";
+        Path inputPath = Paths.get(datasets_base_path, datasetSubPath, "Pos0_GL7", "20220530_VNG1040_AB2h_1_MMStack_Pos0_GL7.tif");
+        Path mmPropertiesPath = Paths.get(datasets_base_path, datasetSubPath, "mm.properties");
+        String analysisName = "debug_analysis__20220905_fix_incorrect_export_folder_path";
+        Integer tmin = null;
+        Integer tmax = 10;
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-p", mmPropertiesPath.toString(), "-analysis", analysisName, "-trackonly"});
     }
 
     public void _20220817_debug_missing_assignments_and_components__test_4__Pos27_GL16() {

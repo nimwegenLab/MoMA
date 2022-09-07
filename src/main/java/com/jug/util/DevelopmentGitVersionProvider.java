92ab6ba90ce29248e1aef6dbfce67e4362dcbee7
Michael Mell <michael.mell@unibas.ch>
2022-09-07 12:45:31 +0200

    Format the Git-output so that fits the format expected by Version.java/Semver.java.

--- src/main/java/com/jug/util/DevelopmentGitVersionProvider.java
+++ src/main/java/com/jug/util/DevelopmentGitVersionProvider.java
@@ -1,5 +1,8 @@
 package com.jug.util;
 
+import com.jug.datahandling.Version;
+import org.apache.commons.lang.NotImplementedException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,19 +11,39 @@ import java.io.InputStreamReader;
  * This class gets a version directly from Git to show a version when running from the IDE during development.
  */
 public class DevelopmentGitVersionProvider implements IVersionProvider {
+    public boolean canReadGitVersionInformation() {
+        try {
+            readGitVersion();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public String getVersionString() {
         try {
-            String var = readGitVersion();
+            String var = formatVersionString(readGitVersion());
             return var;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private String formatVersionString(String versionString){
+        versionString = versionString.substring(1);
+        String[] p = versionString.split("-");
+        if(versionString.contains("dirty")){
+            return p[0]+"-"+p[1]+"-"+p[2]+"-"+p[4]+"+"+p[3];
+        } else {
+            return p[0] + "-" + p[1] + "-" + p[2] + "-" + "+" + p[3];
+        }
+    }
+
     public String readGitVersion() throws IOException
     {
-        String command = "git describe --abbrev=0 --tags";
+//        String command = "git describe --abbrev=0 --tags";
+        String command = "git describe --abbrev=8 --dirty"; /* describes the closest *annotated* tag*/
         Process p = Runtime.getRuntime().exec(command);
         BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
         return input.readLine();

39a20c0cf43a0e8b905a167c97c33377c597261a
Michael Mell <michael.mell@unibas.ch>
2022-10-10 15:23:06 +0200

    Add template for starting a debug-session more quickly.

--- src/test/java/com/jug/exploration/DebuggingExploration.java
+++ src/test/java/com/jug/exploration/DebuggingExploration.java
@@ -46,9 +46,27 @@ public class DebuggingExploration {
 //        tests._2020524_fix_issue_with_non_exported_cell_mask__debug_issue();
     }
 
+    /**
+     * This method template serves quickly create a test-function based on the test-dataset template locate here:
+     * /home/micha/Documents/01_work/15_moma_notes/02_moma_development/bugfix/000__debug_template
+     */
+//    public void __debug_test_method_template__() {
+//        String subfolder = "000__debug_template";
+//        Path inputPath = Paths.get(datasets_base_path, subfolder, "debug_template__gl_data", "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+//        Path properties_file_path = Paths.get(datasets_base_path, subfolder, "mm.properties");
+//        Integer tmin = null;
+//        Integer tmax = 10;
+//        String analysisName = "20221010-fix-gurobi-log-creation";
+//        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+//    }
+
+
+    /**
+     * Actual debugging test-methods are below.
+     */
     public void _20221010_first_run_with_trackonly_does_not_store_gurobi_log_in_analysis_folder__test_trackonly() {
-        String subfolder = "lis_20211026__Pos7_GL12";
-        Path inputPath = Paths.get(datasets_base_path, subfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+        String subfolder = "20221010-first-run-with-trackonly-does-not-store-gurobi-log-in-analysis-folder";
+        Path inputPath = Paths.get(datasets_base_path, subfolder, "debug_template__gl_data", "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasets_base_path, subfolder, "mm.properties");
         Integer tmin = null;
         Integer tmax = 10;
@@ -57,7 +75,7 @@ public class DebuggingExploration {
     }
 
     public void _20221010_first_run_with_trackonly_does_not_store_gurobi_log_in_analysis_folder__test_reloading() {
-        String subfolder = "lis_20211026__Pos7_GL12";
+        String subfolder = "20221010-first-run-with-trackonly-does-not-store-gurobi-log-in-analysis-folder";
         String analysisName = "20221010-fix-gurobi-log-creation";
         Path reload_folder_path = Paths.get(datasets_base_path, subfolder);
         startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});

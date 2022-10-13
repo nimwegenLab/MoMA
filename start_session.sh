1ca9f7a428f873388b4dfdc3cd2d71cdaf447b24
Michael Mell <michael.mell@unibas.ch>
2022-10-13 16:30:50 +0200

    WIP: start work on script to generate a topic session.

--- start_session.sh
+++ start_session.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-SESSION_TYPE=$1
-SESSION_BRANCH=$2
+session_type=$1
+session_branch=$2
 
-printf "%s" "$SESSION_TYPE"
-printf "%s" "$SESSION_BRANCH"
+if [[ ! $session_type ]]; then
+  printf "No session type proved.\n"
+  exit 1
+fi
+
+if [[ ! $session_branch ]]; then
+  printf "No session branch name provided.\n"
+  exit 1
+fi
+
+
+allowed_session_types=("feature" "bugfix" "exploration")
+
+if [[ ! " ${allowed_session_types[*]} " =~ " ${session_type} " ]]; then
+  printf "The provided session type \"%s\" is invalid. Allowed values are: ${allowed_session_types[*]}\n" "${session_type}"
+  exit 1
+fi
+
+#if exists_in_list "$allowed_session_types" " " "$session_type"; then
+#fi
+
+printf "Session type: %s\n" "$session_type"
+printf "Session branch: %s\n" "$session_branch"

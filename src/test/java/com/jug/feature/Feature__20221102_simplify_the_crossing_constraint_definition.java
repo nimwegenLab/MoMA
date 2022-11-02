9b898c03f1959b0e4292a5cb35abebe1992b64eb
Michael Mell <michael.mell@unibas.ch>
2022-11-02 11:10:37 +0100

    "Started new feature-session. Commiting test-class for this session: Feature__20221102_simplify_the_crossing_constraint_definition.java"

---
+++ src/test/java/com/jug/feature/Feature__20221102_simplify_the_crossing_constraint_definition.java
@@ -0,0 +1,58 @@
+package com.jug.feature;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.jug.exploration.ExplorationTestHelpers.startMoma;
+
+
+public class Feature__20221102_simplify_the_crossing_constraint_definition {
+    final String datasetSubfolder;
+    String analysisName;
+    Integer tmin;
+    Integer tmax;
+
+    String datasetsBasePath = "/home/micha/Documents/01_work/15_moma_notes/02_moma_development"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
+
+    public Feature__20221102_simplify_the_crossing_constraint_definition() {
+        datasetSubfolder = "feature/20221102-simplify-the-crossing-constraint-definition"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
+        analysisName = "test_analysis"; /* you can change this if you want to; but it is not needed */
+        tmin = null;
+        tmax = 10;
+    }
+
+    public static void main(String[] args) {
+        Feature__20221102_simplify_the_crossing_constraint_definition tests = new Feature__20221102_simplify_the_crossing_constraint_definition();
+
+        tests.run_interactive();
+//        tests.run_trackonly();
+//        tests.run_reloading();
+//        tests.run_export();
+    }
+
+    /**
+     * Test-methods are below.
+     */
+    public void run_interactive() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
+    }
+
+    public void run_trackonly() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+    }
+
+    public void run_reloading() {
+        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
+        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    }
+
+    public void run_export() {
+        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
+        startMoma(true, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    }
+
+}

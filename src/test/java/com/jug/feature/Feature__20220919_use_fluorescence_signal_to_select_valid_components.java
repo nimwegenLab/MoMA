9cd87a908e230686aea3bde46d78d81e627bf7f8
Michael Mell <michael.mell@unibas.ch>
2022-11-17 12:34:47 +0100

    Test reloading of the results.

--- src/test/java/com/jug/feature/Feature__20220919_use_fluorescence_signal_to_select_valid_components.java
+++ src/test/java/com/jug/feature/Feature__20220919_use_fluorescence_signal_to_select_valid_components.java
@@ -24,8 +24,8 @@ public class Feature__20220919_use_fluorescence_signal_to_select_valid_component
     public static void main(String[] args) {
         Feature__20220919_use_fluorescence_signal_to_select_valid_components tests = new Feature__20220919_use_fluorescence_signal_to_select_valid_components();
 
-//        tests.run_interactive();
-        tests.run_trackonly();
+        tests.run_interactive();
+//        tests.run_trackonly();
 //        tests.run_reloading();
 //        tests.run_export();
     }
@@ -36,12 +36,18 @@ public class Feature__20220919_use_fluorescence_signal_to_select_valid_component
     public void run_interactive() {
         Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_interactive_run"; /* you can change this if you want to; but it is not needed */
+        tmin = 400;
+        tmax = 500;
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
 
     public void run_trackonly() {
         Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_batch_run"; /* you can change this if you want to; but it is not needed */
+        tmin = 400;
+        tmax = 500;
         startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
     }
 

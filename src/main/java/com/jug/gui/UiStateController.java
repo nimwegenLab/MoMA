nimwegenLab/MoMA

commit 4960bbee77784bf2f038287663e4cd69403d39cc

Catch exception and add additional context in AbstractAssignment and Hypothesis.

src/main/java/com/jug/lp/AbstractAssignment.java
@@ -356,6 +356,10 @@ public abstract class AbstractAssignment<H extends Hypothesis<?>> {
 			grbConstr = ilp.model.getConstrByName(constraintName);
 		} catch (GRBException e) {
 			return null;
+		} catch (RuntimeException e){
+			System.out.println("ILP status: " + ilp.getStatus());
+			System.out.println("constraintName: " + constraintName);
+			throw e;
 		}
 		return grbConstr;
 	}

src/main/java/com/jug/lp/GrowthlaneTrackingILP.java
@@ -161,7 +161,19 @@ public class GrowthlaneTrackingILP {
     }
 
     public boolean isReady() {
-        return getStatus() != IlpStatus.OPTIMIZATION_IS_RUNNING && getStatus() != IlpStatus.OPTIMIZATION_NEVER_PERFORMED;
+        return !isOptimizationRunning() && optimizationWasPerformed() && !isInfeasible();
+    }
+
+    public boolean isOptimizationRunning() {
+        return getStatus() == IlpStatus.OPTIMIZATION_IS_RUNNING;
+    }
+
+    public boolean optimizationWasPerformed() {
+        return getStatus() != IlpStatus.OPTIMIZATION_NEVER_PERFORMED;
+    }
+
+    public boolean isInfeasible() {
+        return getStatus() == IlpStatus.INFEASIBLE;
     }
 
     public AssignmentsAndHypotheses<AbstractAssignment<Hypothesis<AdvancedComponent<FloatType>>>, Hypothesis<AdvancedComponent<FloatType>>> getNodes() {

src/main/java/com/jug/lp/Hypothesis.java
@@ -136,6 +136,10 @@ public class Hypothesis<C extends AdvancedComponent<FloatType>> {
             grbConstr = ilp.model.getConstrByName(getSegmentInSolutionConstraintName());
         } catch (GRBException e) {
             return null;
+        } catch (RuntimeException e){
+            System.out.println("ILP status: " + ilp.getStatus());
+            System.out.println("constraintName: " + getSegmentInSolutionConstraintName());
+            throw e;
         }
         return grbConstr;
     }

src/main/java/com/jug/gui/AssignmentEditorPanel.java
@@ -6,6 +6,8 @@ import com.jug.lp.GrowthlaneTrackingILP;
 
 import javax.swing.*;
 
+import static java.util.Objects.isNull;
+
 public class AssignmentEditorPanel extends IlpVariableEditorPanel {
     AssignmentsEditorViewer assignmentView;
     JCheckBox checkboxIsSelected;
@@ -54,7 +56,7 @@ public class AssignmentEditorPanel extends IlpVariableEditorPanel {
         GrowthlaneTrackingILP ilp = momaModel.getCurrentGL().getIlp();
         updateSelectionCheckbox();
 
-        if (ilp == null) {
+        if (isNull(ilp)) {
             assignmentView.display();
             return;
         }

commit bb8ec0280f08072aa7f9d668933a65c3efa5d2a6

Manually revert changes from commit 4960bbee, which broke UI behavior: When forcing assignments and force-ignoring Hypotheses they would no longer be displayed as such.

src/main/java/com/jug/gui/UiStateController.java
@@ -29,10 +29,10 @@ public class UiStateController {
             }
         };
         momaModel.getCurrentGL().addChangeListener(initializationCallback);
-        momaModel.getCurrentGL().addChangeListener(e ->{
-            Growthlane gl = ((Growthlane) e.getSource());
-            sliderPanel.setEnabled(gl.getIlp().isReady() || gl.getIlp().isInfeasible());
-        });
+//        momaModel.getCurrentGL().addChangeListener(e ->{
+//            Growthlane gl = ((Growthlane) e.getSource());
+//            sliderPanel.setEnabled(gl.getIlp().isReady() || gl.getIlp().isInfeasible());
+//        });
     }
 
     ChangeListener initializationCallback;

src/main/java/com/jug/lp/AbstractAssignment.java
@@ -356,10 +356,6 @@ public abstract class AbstractAssignment<H extends Hypothesis<?>> {
 			grbConstr = ilp.model.getConstrByName(constraintName);
 		} catch (GRBException e) {
 			return null;
-		} catch (RuntimeException e){
-			System.out.println("ILP status: " + ilp.getStatus());
-			System.out.println("constraintName: " + constraintName);
-			throw e;
 		}
 		return grbConstr;
 	}

src/main/java/com/jug/lp/GrowthlaneTrackingILP.java
@@ -161,19 +161,7 @@ public class GrowthlaneTrackingILP {
     }
 
     public boolean isReady() {
-        return !isOptimizationRunning() && optimizationWasPerformed() && !isInfeasible();
-    }
-
-    public boolean isOptimizationRunning() {
-        return getStatus() == IlpStatus.OPTIMIZATION_IS_RUNNING;
-    }
-
-    public boolean optimizationWasPerformed() {
-        return getStatus() != IlpStatus.OPTIMIZATION_NEVER_PERFORMED;
-    }
-
-    public boolean isInfeasible() {
-        return getStatus() == IlpStatus.INFEASIBLE;
+        return getStatus() != IlpStatus.OPTIMIZATION_IS_RUNNING && getStatus() != IlpStatus.OPTIMIZATION_NEVER_PERFORMED;
     }
 
     public AssignmentsAndHypotheses<AbstractAssignment<Hypothesis<AdvancedComponent<FloatType>>>, Hypothesis<AdvancedComponent<FloatType>>> getNodes() {

src/main/java/com/jug/lp/Hypothesis.java
@@ -136,10 +136,6 @@ public class Hypothesis<C extends AdvancedComponent<FloatType>> {
             grbConstr = ilp.model.getConstrByName(getSegmentInSolutionConstraintName());
         } catch (GRBException e) {
             return null;
-        } catch (RuntimeException e){
-            System.out.println("ILP status: " + ilp.getStatus());
-            System.out.println("constraintName: " + getSegmentInSolutionConstraintName());
-            throw e;
         }
         return grbConstr;
     }
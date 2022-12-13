fe2cd53f4ef5d1359a379f206eaafe1b648fcfff
Michael Mell <michael.mell@unibas.ch>
2022-12-13 03:29:23 +0100

    AssignmentFilterUsingFluorescencePerFrame: Use intensity ratios of source and target component of the assignment to decide if an assignment is valid.

--- src/main/java/com/jug/lp/AssignmentFilterUsingFluorescencePerFrame.java
+++ src/main/java/com/jug/lp/AssignmentFilterUsingFluorescencePerFrame.java
@@ -36,16 +36,21 @@ public class AssignmentFilterUsingFluorescencePerFrame implements IAssignmentFil
     @Override
     public void evaluate(AbstractAssignment assignment) {
         int frame = assignment.getTargetTimeStep();
-        double intensityMean = imageProperties.getBackgroundIntensityMeanAtFrame(targetChannelNumber, frame);
-        double intensityStd = imageProperties.getBackgroundIntensityStdAtFrame(targetChannelNumber, frame);
-        double threshold = intensityMean + numberOfSigmas * intensityStd;
+//        double intensityMean = imageProperties.getBackgroundIntensityMeanAtFrame(targetChannelNumber, frame);
+//        double intensityStd = imageProperties.getBackgroundIntensityStdAtFrame(targetChannelNumber, frame);
+//        double threshold = intensityMean + numberOfSigmas * intensityStd;
+        Hypothesis<AdvancedComponent<FloatType>> sourceHyp = assignment.getSourceHypothesis();
+        AdvancedComponent<FloatType> sourceComponent = sourceHyp.getWrappedComponent();
+        double sourceComponentIntensityMean = sourceComponent.getMaskIntensityMean(targetChannelNumber);
         List<Hypothesis<AdvancedComponent<FloatType>>> targetHyps = assignment.getTargetHypotheses();
         boolean targetsAreValid = true;
+        double ratio_threshold = numberOfSigmas * 0.068;
         for (Hypothesis<AdvancedComponent<FloatType>> hyp : targetHyps) {
             AdvancedComponent<FloatType> component = hyp.getWrappedComponent();
-            double maskIntensityMean = component.getMaskIntensityMean(targetChannelNumber);
-
-            if (maskIntensityMean < threshold) {
+            double targetComponentIntensityMean = component.getMaskIntensityMean(targetChannelNumber);
+            double intensity_ratio = targetComponentIntensityMean/sourceComponentIntensityMean - 1;
+            if (intensity_ratio < -ratio_threshold || intensity_ratio > ratio_threshold) {
+//            if (!(-ratio_threshold < intensity_ratio && intensity_ratio < ratio_threshold)) {
                 targetsAreValid = false;
             }
         }
--- src/test/java/com/jug/feature/Feature__20221213_use_fluorescence_signal_to_select_valid_components.java
+++ src/test/java/com/jug/feature/Feature__20221213_use_fluorescence_signal_to_select_valid_components.java
@@ -24,10 +24,16 @@ public class Feature__20221213_use_fluorescence_signal_to_select_valid_component
     public static void main(String[] args) {
         Feature__20221213_use_fluorescence_signal_to_select_valid_components tests = new Feature__20221213_use_fluorescence_signal_to_select_valid_components();
 
-        tests.run_interactive();
-//        tests.run_trackonly();
-//        tests.run_reloading();
+//        tests.run_interactive();
+//        tests.run_trackonly__20211026_7_12();
+//        tests.run_reloading__20211026_7_12();
 //        tests.run_export();
+
+//        tests.run_trackonly__20220530_17_6();
+        tests.run_reloading__20220530_17_6();
+
+//        tests.run_trackonly__20220701_10_3();
+//        tests.run_reloading__20220701_10_3();
     }
 
     /**
@@ -40,16 +46,48 @@ public class Feature__20221213_use_fluorescence_signal_to_select_valid_component
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
 
-    public void run_trackonly() {
+    public void run_trackonly__20211026_7_12() {
         Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
-        analysisName = "test_batch_run";
-        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+        analysisName = "test_batch_run__20211026_7_12";
+        tmin = null;
+        tmax = 500;
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
     }
 
-    public void run_reloading() {
+    public void run_reloading__20211026_7_12() {
         Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
+        analysisName = "test_batch_run__20211026_7_12";
+        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    }
+
+    public void run_trackonly__20220530_17_6() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "data/20220530_17_6/20220530_VNG1040_AB2h_1_MMStack_Pos17_GL6.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_batch_run__20220530_17_6";
+        tmin = null;
+        tmax = 480;
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+    }
+
+    public void run_reloading__20220530_17_6() {
+        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder, "data/20220530_17_6");
+        analysisName = "test_batch_run__20220530_17_6";
+        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    }
+
+    public void run_trackonly__20220701_10_3() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "data/20220701_10_3/20220701_VNG1040_AB2h_4_MMStack_Pos0_Pos10_GL3.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_batch_run__20220701_10_3";
+        tmin = null;
+        tmax = 480;
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+    }
+
+    public void run_reloading__20220701_10_3() {
+        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder, "data/20220701_10_3");
+        analysisName = "test_batch_run__20220701_10_3";
         startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
     }
 

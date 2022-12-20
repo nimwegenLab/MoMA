b551ab5d51133ffa67bc75b7457adbf588c62e73
Michael Mell <michael.mell@unibas.ch>
2022-12-20 11:30:31 +0100

    Cleanup.

--- src/main/java/com/jug/lp/AssignmentFilterUsingFluorescencePerFrame.java
+++ src/main/java/com/jug/lp/AssignmentFilterUsingFluorescencePerFrame.java
@@ -35,22 +35,17 @@ public class AssignmentFilterUsingFluorescencePerFrame implements IAssignmentFil
 
     @Override
     public void evaluate(AbstractAssignment assignment) {
-        int frame = assignment.getTargetTimeStep();
-//        double intensityMean = imageProperties.getBackgroundIntensityMeanAtFrame(targetChannelNumber, frame);
-//        double intensityStd = imageProperties.getBackgroundIntensityStdAtFrame(targetChannelNumber, frame);
-//        double threshold = intensityMean + numberOfSigmas * intensityStd;
         Hypothesis<AdvancedComponent<FloatType>> sourceHyp = assignment.getSourceHypothesis();
         AdvancedComponent<FloatType> sourceComponent = sourceHyp.getWrappedComponent();
         double sourceComponentIntensityMean = sourceComponent.getMaskIntensityMean(targetChannelNumber);
         List<Hypothesis<AdvancedComponent<FloatType>>> targetHyps = assignment.getTargetHypotheses();
         boolean targetsAreValid = true;
         double ratio_threshold = numberOfSigmas * 0.068;
-        for (Hypothesis<AdvancedComponent<FloatType>> hyp : targetHyps) {
-            AdvancedComponent<FloatType> component = hyp.getWrappedComponent();
-            double targetComponentIntensityMean = component.getMaskIntensityMean(targetChannelNumber);
+        for (Hypothesis<AdvancedComponent<FloatType>> targetHypothesis : targetHyps) {
+            AdvancedComponent<FloatType> targetComponent = targetHypothesis.getWrappedComponent();
+            double targetComponentIntensityMean = targetComponent.getMaskIntensityMean(targetChannelNumber);
             double intensity_ratio = targetComponentIntensityMean/sourceComponentIntensityMean - 1;
             if (intensity_ratio < -ratio_threshold || intensity_ratio > ratio_threshold) {
-//            if (!(-ratio_threshold < intensity_ratio && intensity_ratio < ratio_threshold)) {
                 targetsAreValid = false;
             }
         }

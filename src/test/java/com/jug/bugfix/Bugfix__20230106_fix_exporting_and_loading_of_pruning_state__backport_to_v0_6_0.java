ef98db653bdca1ddc002c0495862d2c5a7b033d7
Michael Mell <michael.mell@unibas.ch>
2023-01-11 12:59:08 +0100

    Increase testing time-range.

--- src/test/java/com/jug/bugfix/Bugfix__20230106_fix_exporting_and_loading_of_pruning_state__backport_to_v0_6_0.java
+++ src/test/java/com/jug/bugfix/Bugfix__20230106_fix_exporting_and_loading_of_pruning_state__backport_to_v0_6_0.java
@@ -18,7 +18,7 @@ public class Bugfix__20230106_fix_exporting_and_loading_of_pruning_state__backpo
         datasetSubfolder = "bugfix/20230106-fix-exporting-and-loading-of-pruning-state--backport_to_v0_6_0"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
         analysisName = "test_analysis"; /* you can change this if you want to; but it is not needed */
         tmin = null;
-        tmax = 10;
+        tmax = 20;
     }
 
     public static void main(String[] args) {
@@ -44,7 +44,7 @@ public class Bugfix__20230106_fix_exporting_and_loading_of_pruning_state__backpo
         Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
         analysisName = "test_batch_run";
-        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
     }
 
     public void run_reloading() {

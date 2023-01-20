de6176ca86924ef5332c524ab6fcc5999ca78681
Michael Mell <michael.mell@unibas.ch>
2023-01-20 12:40:41 +0100

    Loading Theos data now works.

--- src/main/java/com/jug/datahandling/VersionCompatibilityChecker.java
+++ src/main/java/com/jug/datahandling/VersionCompatibilityChecker.java
@@ -5,23 +5,44 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class VersionCompatibilityChecker {
-    private static HashMap<Version, Set<Version>> versionCompatibility;
+    private static HashMap<String, Set<String>> datasetCompatibility;
     static {
-        versionCompatibility = new HashMap<>();
-
-        versionCompatibility.put(
-                new Version("0.9"),
+        datasetCompatibility = new HashMap<>(); /* this maps the MoMA version (the map key) to compatible dataset-version (the mapped set) */
+        datasetCompatibility.put(
+                "0.9",
                 new HashSet() {{
-                    add(new Version("0.9"));
-                    add(new Version("0.7"));
+                    add("0.9");
+                    add("0.7");
                 }});
     }
 
+    public Set<String> getCompatibleDatasetVersions(String momaVersion) throws NoVersionFoundException {
+        if (!datasetCompatibility.containsKey(momaVersion)) {
+            throw new NoVersionFoundException();
+        }
+        return datasetCompatibility.get(momaVersion);
+    }
+
     public boolean versionAreCompatible(Version momaVersionOfRunningInstance, Version momaVersionOfDataset) {
-        return momaVersionOfDataset.getMinor() == momaVersionOfRunningInstance.getMinor(); /* TODO: for the moment we enforce that the dataset-version must be of same minor-version, because MoMA is not yet stable */
+        String versionString = getVersionString(momaVersionOfRunningInstance);
+        Set<String> compatibleDataVersions;
+        try {
+            compatibleDataVersions = getCompatibleDatasetVersions(versionString);
+        } catch (NoVersionFoundException e) {
+            return false;
+        }
+        String datasetVersionString = getVersionString(momaVersionOfDataset);
+        boolean versionIsCompatible = compatibleDataVersions.contains(datasetVersionString);
+        return versionIsCompatible;
+    }
+
+    private String getVersionString(Version version) {
+        return String.format("%d.%d", version.getMajor(), version.getMinor());
     }
 
     public String getErrorMessage(Version momaVersionOfRunningInstance, Version momaVersionOfDataset) {
         return "\tDataset version is incompatible:\n\tDataset version: " + momaVersionOfDataset.toString() + "\n\tMoMA version: " + momaVersionOfRunningInstance.toString();
     }
+
+    class NoVersionFoundException extends Exception { }
 }

c5f540061cbece9f00b4899227fcea3283904a17
Michael Mell <michael.mell@unibas.ch>
2023-01-21 23:30:29 +0100

    Handle the missing value-fields for maskIntensities and backgroundIntensities in AdvancedComponentPojo. I think this is the place, where it should be handled, because it is a concern of the deserialization ...

--- src/main/java/com/jug/util/componenttree/AdvancedComponentPojo.java
+++ src/main/java/com/jug/util/componenttree/AdvancedComponentPojo.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.isNull;
+
 public class AdvancedComponentPojo {
     private final String stringId;
     private final int frameNumber;
@@ -96,10 +98,16 @@ public class AdvancedComponentPojo {
     }
 
     public Map<Integer, Double> getMaskIntensities() {
+        if (isNull(maskIntensities)) {
+            maskIntensities = new HashMap<>();
+        }
         return maskIntensities;
     }
 
     public Map<Integer, Double> getBackgroundIntensities() {
+        if (isNull(backgroundIntensities)) {
+            backgroundIntensities = new HashMap<>();
+        }
         return backgroundIntensities;
     }
 

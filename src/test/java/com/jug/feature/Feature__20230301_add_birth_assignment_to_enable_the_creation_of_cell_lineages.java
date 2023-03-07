0c78df78f9fc07b8e22b706fd611cd6e571f2c4b
Michael Mell <michael.mell@unibas.ch>
2023-03-07 12:04:37 +0100

    WIP: Add tests for testing the loading of EnterAssignments.

--- src/test/java/com/jug/feature/Feature__20230301_add_birth_assignment_to_enable_the_creation_of_cell_lineages.java
+++ src/test/java/com/jug/feature/Feature__20230301_add_birth_assignment_to_enable_the_creation_of_cell_lineages.java
@@ -24,7 +24,9 @@ public class Feature__20230301_add_birth_assignment_to_enable_the_creation_of_ce
     public static void main(String[] args) {
         Feature__20230301_add_birth_assignment_to_enable_the_creation_of_cell_lineages tests = new Feature__20230301_add_birth_assignment_to_enable_the_creation_of_cell_lineages();
 
-        tests.run_interactive__lis_20221102_2_29();
+//        tests.run_interactive__lis_20221102_2_29();
+//        tests.run_trackonly__lis_20221102_2_29();
+        tests.run_reloading__lis_20221102_2_29();
     }
 
     /**
@@ -37,5 +39,20 @@ public class Feature__20230301_add_birth_assignment_to_enable_the_creation_of_ce
         tmin = null;
         tmax = 30;
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
+    }
+
+    public void run_trackonly__lis_20221102_2_29() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "/data/lis_20221102_2_29/20221102_VNG1040_SHU_1_MMStack_Pos2_GL29.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "/data/lis_20221102_2_29/mm.properties");
+        analysisName = "test_reloading__lis_20221102_2_29";
+        tmin = null;
+        tmax = 30;
+        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+    }
+
+    public void run_reloading__lis_20221102_2_29() {
+        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder, "/data/lis_20221102_2_29");
+        analysisName = "test_reloading__lis_20221102_2_29";
+        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
     }
 }

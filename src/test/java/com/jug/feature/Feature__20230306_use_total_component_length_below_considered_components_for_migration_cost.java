deba733e24d759f076bac9177bbe074548b2a17f
Michael Mell <michael.mell@unibas.ch>
2023-03-08 09:43:04 +0100

    Add test to analyze problematic behavior, when cells jump move downwards into the GL.

--- src/test/java/com/jug/feature/Feature__20230306_use_total_component_length_below_considered_components_for_migration_cost.java
+++ src/test/java/com/jug/feature/Feature__20230306_use_total_component_length_below_considered_components_for_migration_cost.java
@@ -24,39 +24,18 @@ public class Feature__20230306_use_total_component_length_below_considered_compo
     public static void main(String[] args) {
         Feature__20230306_use_total_component_length_below_considered_components_for_migration_cost tests = new Feature__20230306_use_total_component_length_below_considered_components_for_migration_cost();
 
-        tests.run_interactive();
-//        tests.run_trackonly();
-//        tests.run_reloading();
-//        tests.run_export();
+        tests.run_interactive__lis_20221102_2_29();
     }
 
     /**
      * Test-methods are below.
      */
-    public void run_interactive() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
-        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
-        analysisName = "test_interactive";
+    public void run_interactive__lis_20221102_2_29() {
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "/data/lis_20221102_2_29/20221102_VNG1040_SHU_1_MMStack_Pos2_GL29.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "/data/lis_20221102_2_29/mm.properties");
+        analysisName = "test_enter_assignment";
+        tmin = 6;
+        tmax = 30;
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
-
-    public void run_trackonly() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
-        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
-        analysisName = "test_batch_run";
-        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
-    }
-
-    public void run_reloading() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
-    }
-
-    public void run_export() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(true, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
-    }
-
 }

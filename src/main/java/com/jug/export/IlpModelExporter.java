be7c94a968eba4b70e77a2d0ef25fa91c928ca05
Michael Mell <michael.mell@unibas.ch>
2023-03-17 11:24:47 +0100

    Add new method to check if storage locks need to be removed, before doing so.

--- src/main/java/com/jug/export/IlpModelExporter.java
+++ src/main/java/com/jug/export/IlpModelExporter.java
@@ -3,12 +3,16 @@ package com.jug.export;
 import com.jug.Growthlane;
 import com.jug.datahandling.IGlExportFilePathGetter;
 import com.jug.lp.GRBModel.IGRBModelAdapter;
+import com.jug.lp.GrowthlaneTrackingILP;
 import gurobi.GRBException;
 
 public class IlpModelExporter implements ResultExporterInterface {
     @Override
     public void export(Growthlane gl, IGlExportFilePathGetter exportFilePaths) throws GRBException {
-        gl.getIlp().addStorageLockConstraintsToAssignments();
+        GrowthlaneTrackingILP ilp = gl.getIlp();
+        if (!ilp.modelIsLockedForStorage()) {
+            ilp.addStorageLockConstraintsToAssignments();
+        }
         IGRBModelAdapter model = gl.getIlp().model;
         model.write(exportFilePaths.getGurobiLpFilePath().toString());
         model.write(exportFilePaths.getGurobiMpsFilePath().toString());

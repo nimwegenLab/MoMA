22d26b3479db3d946f8e5e8d454eef31be212054
Michael Mell <michael.mell@unibas.ch>
2023-03-23 11:33:11 +0100

    WIP: Implement integration test to test data export.
    - Add test class DataExportIntegrationTest.java

---
+++ src/test/java/com/jug/integration/DataExportIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.jug.integration;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.jug.exploration.ExplorationTestHelpers.startMoma;
+
+public class DataExportIntegrationTest {
+    @Test
+    public void tracking_and_exporting_yields_same_result() throws IOException {
+        Path temporaryWorkingDirectory = null;
+        temporaryWorkingDirectory = Files.createTempDirectory("java-DataExportIntegrationTest-");
+
+        /* create and save model */
+//        pathToTempDir
+//        System.out.println("Temporary Directory: " + pathToTempDir.toAbsolutePath());
+
+        Path testDataSourcePath = Paths.get(new File("").getAbsolutePath(), "src/test/resources/test/integration/lis_20221102_2_29/");
+
+        FileUtils.copyDirectory(testDataSourcePath.toFile(), temporaryWorkingDirectory.toFile());
+        System.out.println("stop");
+
+        Path inputImagePath = Paths.get(temporaryWorkingDirectory.toString(), "20221102_VNG1040_SHU_1_MMStack_Pos2_GL29.tif");
+        Path propertiesFilePath = Paths.get(temporaryWorkingDirectory.toString(), "mm.properties");
+        Path referenceDataPath = Paths.get(temporaryWorkingDirectory.toString(), "reference_output_data");
+        String analysisName = "test_analysis";
+        Integer tmin = null;
+        Integer tmax = null;
+        startMoma(false, inputImagePath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", propertiesFilePath.toString(), "-analysis", analysisName});
+    }
+}

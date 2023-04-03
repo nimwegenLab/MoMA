6d0952699a80078498e323935b016aaf0d233fc7
Michael Mell <michael.mell@unibas.ch>
2023-04-03 15:26:29 +0200

    Add tests.

--- src/test/java/com/jug/feature/Feature__20230330_2_improve_tracking_performance_at_detection_roi_border.java
+++ src/test/java/com/jug/feature/Feature__20230330_2_improve_tracking_performance_at_detection_roi_border.java
@@ -24,39 +24,52 @@ public class Feature__20230330_2_improve_tracking_performance_at_detection_roi_b
     public static void main(String[] args) {
         Feature__20230330_2_improve_tracking_performance_at_detection_roi_border tests = new Feature__20230330_2_improve_tracking_performance_at_detection_roi_border();
 
-        tests.run_interactive();
-//        tests.run_trackonly();
-//        tests.run_reloading();
-//        tests.run_export();
+//        tests.run_interactive__lis_20220530_19_7();
+//        tests.run_interactive__lis_20211026_7_12();
+        tests.run_interactive__theo_20221220_25_36();
+//        tests.run_interactive__theo_20221220_28_35();
     }
 
     /**
      * Test-methods are below.
      */
-    public void run_interactive() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+    public void run_interactive__lis_20220530_19_7() {
+        tmin = null;
+        tmax = 100;
+        String subDir = "data/lis_20220530_19_7";
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, subDir,  "20220530_VNG1040_AB2h_1_MMStack_Pos19_GL7.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
         analysisName = "test_interactive";
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
 
-    public void run_trackonly() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+    public void run_interactive__lis_20211026_7_12() {
+        tmin = 300;
+        tmax = 400;
+        String subDir = "data/lis_20211026_7_12";
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, subDir,  "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
-        analysisName = "test_batch_run";
-        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
+        analysisName = "test_interactive";
+        startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
 
-    public void run_reloading() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    public void run_interactive__theo_20221220_25_36() {
+        tmin = 300;
+        tmax = 400;
+        String subDir = "data/theo_20221220_25_36";
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, subDir,  "20221220_glu_spcm_1_MMStack_Pos25_GL36.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_interactive";
+        startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
 
-    public void run_export() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(true, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
+    public void run_interactive__theo_20221220_28_35() {
+        tmin = 300;
+        tmax = 400;
+        String subDir = "data/theo_20221220_28_35";
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, subDir,  "20221220_glu_spcm_1_MMStack_Pos28_GL35.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_interactive";
+        startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
-
 }

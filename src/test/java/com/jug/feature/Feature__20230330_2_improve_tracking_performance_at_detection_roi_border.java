80614d7c5ea7a26773c4762b5b877fc0e01c5009
Michael Mell <michael.mell@unibas.ch>
2023-04-03 23:25:29 +0200

    add additional test.

--- src/test/java/com/jug/feature/Feature__20230330_2_improve_tracking_performance_at_detection_roi_border.java
+++ src/test/java/com/jug/feature/Feature__20230330_2_improve_tracking_performance_at_detection_roi_border.java
@@ -24,21 +24,31 @@ public class Feature__20230330_2_improve_tracking_performance_at_detection_roi_b
     public static void main(String[] args) {
         Feature__20230330_2_improve_tracking_performance_at_detection_roi_border tests = new Feature__20230330_2_improve_tracking_performance_at_detection_roi_border();
 
-        tests.run_interactive__theo_20221220_25_36();
+//        tests.run_interactive__theo_20221220_25_36_400ff();
+//        tests.run_interactive__theo_20221220_25_36_940ff();
 //        tests.run_interactive__theo_20221220_28_35();
 //        tests.run_interactive__lis_20211026_7_12();
 //        tests.run_interactive__lis_20220530_11_29();
 //        tests.run_interactive__lis_20220530_12_7();
 //        tests.run_interactive__lis_20220530_12_26();
 //        tests.run_interactive__lis_20220530_18_2();
-//        tests.run_interactive__lis_20220530_19_7_175ff();
+        tests.run_interactive__lis_20220530_19_7_175ff();
 //        tests.run_interactive__lis_20220530_19_7_75ff();
     }
 
     /**
      * Test-methods are below.
      */
-    public void run_interactive__theo_20221220_25_36() {
+    public void run_interactive__theo_20221220_25_36_940ff() {
+        tmin = 940;
+        tmax = 955;
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "data/theo_20221220_25_36/20221220_glu_spcm_1_MMStack_Pos25_GL36.tif");
+        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
+        analysisName = "test_interactive";
+        startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
+    }
+
+    public void run_interactive__theo_20221220_25_36_400ff() {
         tmin = 400;
         tmax = 500;
         Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "data/theo_20221220_25_36/20221220_glu_spcm_1_MMStack_Pos25_GL36.tif");

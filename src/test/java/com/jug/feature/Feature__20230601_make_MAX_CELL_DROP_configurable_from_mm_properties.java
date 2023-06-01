ff1b9e090ff03ec205b182ea1efec1bc8be828e4
Michael Mell <michael.mell@unibas.ch>
2023-06-01 12:50:03 +0200

    Reproduce issue of missing assignment for cell that jumps down.

--- src/test/java/com/jug/feature/Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_properties.java
+++ src/test/java/com/jug/feature/Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_properties.java
@@ -15,48 +15,25 @@ public class Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_propertie
     String datasetsBasePath = "/home/micha/Documents/01_work/15_moma_notes/02_moma_development"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
 
     public Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_properties() {
-        datasetSubfolder = "feature/20230601-make-MAX_CELL_DROP-configurable-from-mm-properties"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
+        datasetSubfolder = "feature/20230601-make-MAX_CELL_DROP-configurable-from-mm-properties/data/Pos0_GL9"; /* DO NOT CHANGE: value is overwritten by the script start_topic_branch.sh, which uses this template to create a session for e.g. feature-development or bug-fixing */
         analysisName = "test_analysis"; /* you can change this if you want to; but it is not needed */
-        tmin = null;
-        tmax = 10;
+        tmin = 100;
+        tmax = 110;
     }
 
     public static void main(String[] args) {
         Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_properties tests = new Feature__20230601_make_MAX_CELL_DROP_configurable_from_mm_properties();
 
         tests.run_interactive();
-//        tests.run_trackonly();
-//        tests.run_reloading();
-//        tests.run_export();
     }
 
     /**
      * Test-methods are below.
      */
     public void run_interactive() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
+        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20220320_VNG1040_AB2h_1_Frame0-478_resaved_MMStack_Pos0_Pos0_GL9.tif");
         Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
         analysisName = "test_interactive";
         startMoma(false, inputPath.toString(), null, tmin, tmax, false, new String[]{"-f", "-p", properties_file_path.toString(), "-analysis", analysisName});
     }
-
-    public void run_trackonly() {
-        Path inputPath = Paths.get(datasetsBasePath, datasetSubfolder, "20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif");
-        Path properties_file_path = Paths.get(datasetsBasePath, datasetSubfolder, "mm.properties");
-        analysisName = "test_batch_run";
-        startMoma(true, inputPath.toString(), null, tmin, tmax, false, new String[]{"-headless", "-p", properties_file_path.toString(), "-analysis", analysisName, "-trackonly"});
-    }
-
-    public void run_reloading() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(false, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
-    }
-
-    public void run_export() {
-        Path reload_folder_path = Paths.get(datasetsBasePath, datasetSubfolder);
-        analysisName = "test_batch_run";
-        startMoma(true, null, null, null, null, false, new String[]{"-analysis", analysisName, "-reload", reload_folder_path.toString()});
-    }
-
 }

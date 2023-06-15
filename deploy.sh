d080f7543b14611c69e414b5f5fe3263b73466f5
Michael Mell <michael.mell@unibas.ch>
2023-06-15 14:06:16 +0200

    Change local maven repository name.

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ GRB_VERSION="10.0.2"
 
 mvn clean install:install-file -Dfile=lib/jmathplot.jar -DgroupId=jmathplot -DartifactId=jmathplot -Dversion=1.0 -Dpackaging=jar | tee "$BUILDLOG"
 
-mvn clean install:install-file -Dfile=lib/gurobi.jar -DgroupId=gurobi -DartifactId=gurobi-jar -Dversion="$GRB_VERSION" -Dpackaging=jar -DlocalRepositoryPath=$(pwd)/maven-repository | tee -a "$BUILDLOG"
-#mvn clean install:install-file -Dfile=lib/gurobi.jar -DgroupId=local.gurobi -DartifactId=gurobi-jar -Dversion=9.5.2 -Dpackaging=jar | tee -a "$BUILDLOG"
+mvn clean install:install-file -Dfile=lib/gurobi.jar -DgroupId=gurobi -DartifactId=gurobi-jar -Dversion="$GRB_VERSION" -Dpackaging=jar -DlocalRepositoryPath="$(pwd)/maven-local-repository" | tee -a "$BUILDLOG"
 
 mvn -Dmaven.test.skip=true -Denforcer.skip package | tee "$BUILDLOG"

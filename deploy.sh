a1c0f4eb5a42ac34a580088b05295a2ced12131a
Michael Mell <michael.mell@unibas.ch>
2023-06-15 10:48:17 +0200

    Output build logs from maven.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
-mvn clean install:install-file -Dfile=lib/jmathplot.jar -DgroupId=jmathplot -DartifactId=jmathplot -Dversion=1.0 -Dpackaging=jar
+#!/usr/bin/env bash
 
-mvn clean install:install-file -Dfile=lib/gurobi.jar -DgroupId=gurobi -DartifactId=gurobi -Dversion=1.0 -Dpackaging=jar
+BUILDLOG="$(pwd)/maven_logs/build_log__$(date +"%Y%m%d-%H%M%S").txt"
+mkdir -p maven_logs
+touch "$BUILDLOG"
 
-mvn -Dmaven.test.skip=true -Denforcer.skip package
+mvn clean install:install-file -Dfile=lib/jmathplot.jar -DgroupId=jmathplot -DartifactId=jmathplot -Dversion=1.0 -Dpackaging=jar | tee "$BUILDLOG"
+
+mvn clean install:install-file -Dfile=lib/gurobi.jar -DgroupId=gurobi -DartifactId=gurobi -Dversion=1.0 -Dpackaging=jar -DlocalRepositoryPath=$(pwd)/repo | tee -a "$BUILDLOG"
+
+mvn -Dmaven.test.skip=true -Denforcer.skip package | tee "$BUILDLOG"
 

a7d81610ea323c7c1238657217607ff2a32b215e
Michael Mell <michael.mell@unibas.ch>
2023-07-25 13:13:43 +0200

    Got the tests to pass.

--- src/main/java/com/jug/export/ImageFileNameParser.java
+++ src/main/java/com/jug/export/ImageFileNameParser.java
@@ -6,33 +6,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ImageFileNameParser {
+    String positionId;
+    String glId;
+
     public void parseFileName(String filename) {
-//        Pattern positionPattern = Pattern.compile("([-,1-9]*Pos\\d+)_GL[0-9]*.tif");
         Pattern positionPattern = Pattern.compile("([-1-9]*Pos\\d+)_(GL[0-9]*)\\.tif");
-//        imagePath = Paths.get(configurationManager.getInputImagePath());
         Matcher positionMatcher = positionPattern.matcher(filename);
         positionMatcher.find();
         positionMatcher.group();
-        String positionId = positionMatcher.group(0); // group(0) is the whole match; group(1) is just the number, which is what we want
-        String GlId = positionMatcher.group(1); // group(0) is the whole match; group(1) is just the number, which is what we want
-        String positionNumber = positionMatcher.group(1); // group(0) is the whole match; group(1) is just the number, which is what we want
-
-        Pattern growthlanePattern = Pattern.compile("GL(\\d+)");
-        Matcher growthlaneMatcher = growthlanePattern.matcher(filename);
-        growthlaneMatcher.find();
-        String growthlaneNumber = growthlaneMatcher.group(1); // group(0) is the whole match; group(1) is just the number, which is what we want
-
-        String laneID = "pos_" + positionNumber + "_GL_" + growthlaneNumber;
+        positionId = positionMatcher.group(1); // group(0) is the whole match; group(1) is just the number, which is what we want
+        glId = positionMatcher.group(2); // group(0) is the whole match; group(1) is just the number, which is what we want
     }
 
-    String getPositionNumber() {
-//        return laneID;
-        throw new NotImplementedException();
+    String getPositionId() {
+        return positionId;
     }
 
 
-    String getGlNumber() {
-//        return laneID;
-        throw new NotImplementedException();
+    String getGlId() {
+        return glId;
     }
 }
--- src/test/java/com/jug/export/ImageFileNameParserTest.java
+++ src/test/java/com/jug/export/ImageFileNameParserTest.java
@@ -1,16 +1,23 @@
 package com.jug.export;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class ImageFileNameParserTest {
 
     @Test
-    public void testParseImageFileName() {
+    public void getPositionId__file_name_variant_1__positionId_is_correct() {
         ImageFileNameParser sut = new ImageFileNameParser();
-        Path imagePath = Paths.get("/home/micha/Documents/01_work/15_moma_notes/02_moma_development/bugfix/20230725-fix-csv-position-column-header-for-new-position-naming-scheme/wt_zwf_oxi_rep2_1_MMStack__8-Pos001_GL29.tif");
-        sut.parseFileName(imagePath.getFileName().toString());
+//        Path imagePath = Paths.get("/home/micha/Documents/01_work/15_moma_notes/02_moma_development/bugfix/20230725-fix-csv-position-column-header-for-new-position-naming-scheme/wt_zwf_oxi_rep2_1_MMStack__8-Pos001_GL29.tif");
+//        sut.parseFileName(imagePath.getFileName().toString());
+        sut.parseFileName("wt_zwf_oxi_rep2_1_MMStack__8-Pos001_GL29.tif");
+        Assertions.assertEquals("8-Pos001", sut.getPositionId());
+    }
+
+    @Test
+    public void getPositionId__file_name_variant_1__glId_is_correct() {
+        ImageFileNameParser sut = new ImageFileNameParser();
+        sut.parseFileName("wt_zwf_oxi_rep2_1_MMStack__8-Pos001_GL29.tif");
+        Assertions.assertEquals("GL29", sut.getGlId());
     }
 }

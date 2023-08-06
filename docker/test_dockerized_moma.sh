f19b13b863ef50a395843c00ce51ba4a56dac22a
Michael Mell <michael.mell@unibas.ch>
2023-08-07 01:14:24 +0200

    Got MoMA to work inside container with the '-trackonly' option:
    - Use xvfb-run inside moma_in_container.sh to avoid headless error. It thus runs without UI (see test_dockerized_moma.sh).

--- docker/test_dockerized_moma.sh
+++ docker/test_dockerized_moma.sh
@@ -2,4 +2,10 @@
 
 export GRB_LICENSE_FILE="/home/micha/Documents/LicenseFiles/gurobi_web_license.lic"
 base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma"
-./moma -p "${base_path}/mm.properties" -tmax 201 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+
+# corresponds to command inside container:
+# /moma/moma_in_container.sh -p /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/mm.properties -tmax 10 -analysis slurm_test_3 -headless -trackonly -i /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif
+# xvfb-run /moma/moma_in_container.sh -p /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/mm.properties -tmax 10 -analysis slurm_test_3 -headless -trackonly -i /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif
+
+./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+

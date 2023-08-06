7a2e41ea87286bb36a72a08f673fa9bb7c3347d2
Michael Mell <michael.mell@unibas.ch>
2023-08-07 00:43:53 +0200

    Mount GRB_LICENSE_FILE and export variable inside container. Add function to append paths to variable 'mount_string'

--- docker/test_dockerized_moma.sh
+++ docker/test_dockerized_moma.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+export GRB_LICENSE_FILE="/home/micha/Documents/LicenseFiles/gurobi_web_license.lic"
 base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma"
 ./moma -p "${base_path}/mm.properties" -tmax 201 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"

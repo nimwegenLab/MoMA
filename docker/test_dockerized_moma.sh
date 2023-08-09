114ba00eb02ead990ab3454ec46ba9755c444499
Michael Mell <michael.mell@unibas.ch>
2023-08-09 14:59:06 +0200

    Remove mounting of temporary directory, which was used for caching the TF models, since we now do this inside the container.

--- docker/test_dockerized_moma.sh
+++ docker/test_dockerized_moma.sh
@@ -8,8 +8,8 @@ base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/featu
 # xvfb-run /moma/moma_in_container.sh -p /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/mm.properties -tmax 10 -analysis slurm_test_3 -headless -trackonly -i /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif
 
 # command for testing the "TRACK" stage of the batch-run workflow:
-#export DISPLAY=""
-#./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+export DISPLAY=""
+./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
 
 # command for testing the "CURATE" stage of the batch-run workflow:
-./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -rl "${base_path}"
+#./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -rl "${base_path}"

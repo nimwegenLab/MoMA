7146a5a1303c4969914903f87208ddcc9e881908
Michael Mell <michael.mell@unibas.ch>
2023-08-09 14:50:23 +0200

    WIP: Adapt changes from script `moma` on HPC cluster, so that it runs inside the container.

--- docker/moma_in_container.sh
+++ docker/moma_in_container.sh
@@ -18,6 +18,14 @@ if [ ! -f "$CONFIG_FILE" ]; then
   printf "Config file did not exist: $CONFIG_FILE\nCreated it.\n"
 fi
 
+MOMA_TMP_WORK_DIR=$(mktemp -d -t moma.$(date +%Y%m%d-%H%M%S).XXXXX)
+MOMA_TMP_WORK_DIR_TF="${MOMA_TMP_WORK_DIR}/tensorflow"
+JAVA_TMPDIR="${MOMA_TMP_WORK_DIR}/java.io.tmpdir"
+mkdir -p "${MOMA_TMP_WORK_DIR_TF}"
+mkdir -p "${JAVA_TMPDIR}"
+printf "MOMA_TMP_WORK_DIR: ${MOMA_TMP_WORK_DIR}\n"
+printf "MOMA_TMP_WORK_DIR_TF: ${MOMA_TMP_WORK_DIR_TF}\n"
+printf "JAVA_TMPDIR: ${JAVA_TMPDIR}\n"
 
 #cd $MOMA_HOME
 # Notes:
@@ -38,9 +46,25 @@ MOMA_JAR_FILENAME=$(ls "${MOMA_JAR_PATH}"/*.jar | grep -P '^((?!original|tests|s
 #echo java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
 #xvfb-run java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
 if [[ -z "${DISPLAY}" ]]; then
-  xvfb-run java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
+#  xvfb-run java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
+xvfb-run java -Xmx24g \
+     -Djava.io.tmpdir=$JAVA_TMPDIR \
+     -Djava.library.path=$GUROBI_LIB_PATH:$TF_JAVA_LIB_PATH \
+     -Dimagej.tensorflow.models.dir="${MOMA_TMP_WORK_DIR_TF}" \
+     -Djava.util.prefs.systemRoot="${MOMA_TMP_WORK_DIR}" \
+     -Djava.util.prefs.userRoot="${MOMA_TMP_WORK_DIR}" \
+     -jar  "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" \
+     "$@"
 else
-  java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
+#  java -Xmx32g -Djava.library.path="${GUROBI_LIB_PATH}":"${TF_JAVA_LIB_PATH}" -jar "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" "$@"
+java -Xmx24g \
+     -Djava.io.tmpdir=$JAVA_TMPDIR \
+     -Djava.library.path=$GUROBI_LIB_PATH:$TF_JAVA_LIB_PATH \
+     -Dimagej.tensorflow.models.dir="${MOMA_TMP_WORK_DIR_TF}" \
+     -Djava.util.prefs.systemRoot="${MOMA_TMP_WORK_DIR}" \
+     -Djava.util.prefs.userRoot="${MOMA_TMP_WORK_DIR}" \
+     -jar  "${MOMA_JAR_PATH}"/"${MOMA_JAR_FILENAME}" \
+     "$@"
 fi
 
 #cd -

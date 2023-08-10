8b41f0fe9d47c50c01a15dd6c292d877105e508e
Michael Mell <michael.mell@unibas.ch>
2023-08-10 13:10:24 +0200

    Add long arguments to parse the path for.

--- docker/test_dockerized_moma.sh
+++ docker/test_dockerized_moma.sh
@@ -9,7 +9,7 @@ base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/featu
 # xvfb-run /moma/moma_in_container.sh -p /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/mm.properties -tmax 10 -analysis slurm_test_3 -headless -trackonly -i /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20230612-containerize-moma/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif
 
 # run moma without arguments
-./moma
+#./moma
 
 # run moma with help argument
 #./moma --help
@@ -17,6 +17,10 @@ base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/featu
 # command for testing the "TRACK" stage of the batch-run workflow:
 #export DISPLAY=""
 #./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+
+# command for testing the "TRACK" stage of the batch-run workflow with long arguments:
+export DISPLAY=""
+./moma --props "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly --infolder "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
 
 # command for testing the "CURATE" stage of the batch-run workflow:
 #./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -rl "${base_path}"

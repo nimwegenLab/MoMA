60f62131aaf8590fa371abef074a4ce46f288426
Michael Mell <michael.mell@unibas.ch>
2023-08-10 14:00:19 +0200

    Only parse paths, if arguments were passed.

--- docker/test_dockerized_moma.sh
+++ docker/test_dockerized_moma.sh
@@ -12,15 +12,15 @@ base_path="/home/micha/Documents/01_work/15_moma_notes/02_moma_development/featu
 #./moma
 
 # run moma with help argument
-#./moma --help
+#./moma -help
 
 # command for testing the "TRACK" stage of the batch-run workflow:
 #export DISPLAY=""
-#./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly -i "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
 
 # command for testing the "TRACK" stage of the batch-run workflow with long arguments:
-export DISPLAY=""
-./moma --props "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly --infolder "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
+#export DISPLAY=""
+#./moma --props "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -headless -trackonly --infolder "${base_path}/20211026_VNG1040_AB6min_2h_1_MMStack_Pos7_GL12.tif"
 
 # command for testing the "CURATE" stage of the batch-run workflow:
 #./moma -p "${base_path}/mm.properties" -f -tmax 10 -analysis slurm_test_3 -rl "${base_path}"

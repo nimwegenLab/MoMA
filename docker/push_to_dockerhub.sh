e0842f9178e5b9825b90ca495051761192ba90f4
Michael Mell <michael.mell@unibas.ch>
2023-08-16 18:51:59 +0200

    Add script to push to docker hub.

---
+++ docker/push_to_dockerhub.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function get_version() {
+  # Check if the current commit is tagged with a version number. If not return "dev". If yes return the version number.
+  # Fail, if it is tagged with an non-version tag.
+  if [[ $(git describe) =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    # catch strings like e.g.: v0.9.7
+    git describe
+  elif [[ $(git describe) =~ ^v[0-9]+\.[0-9]+\.[0-9]+-[0-9]+-.*$ ]]; then
+    # catch strings like e.g.: v0.9.7-5-g46d4d956
+    echo "dev"
+  else
+    # fail for other values
+    exit 1
+  fi
+}
+
+version=$(get_version)
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unknown tag: $(git describe)" >&2
+    exit 1
+fi
+
+IMAGE_TAG="michaelmell/moma:${version}"
+
+echo "Pushing to Dockerhub image: ${IMAGE_TAG}"
+docker push "${IMAGE_TAG}"

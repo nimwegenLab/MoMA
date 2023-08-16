7531f481be0a7f76dda3f70c893e2eaa7d847be3
Michael Mell <michael.mell@unibas.ch>
2023-08-16 17:09:39 +0200

    remove helper.sh: its function are not used. define get_version() in build_docker_image.sh, which gets the version from the last Git tag.

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-source helpers.sh
+function get_version() {
+  # Check if the current commit is tagged with a version number. If not return "dev". If yes return the version number.
+  # Fail, if it is tagged with an non-version tag.
+  if [[ $(git describe) =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    # catch strings like e.g.: v0.9.7
+    git describe
+  elif [[ $(git describe) =~ ^v[0-9]+\.[0-9]+\.[0-9]+-[0-9]+-.*$ ]]; then
+    # catch strings like e.g.: v0.9.7-5-g46d4d956
+    echo "dev"
+  else
+    # fail for other values
+    exit 1
+  fi
+}
 
-#IMAGE_TAG=$(get_image_tag)
-IMAGE_TAG="michaelmell/moma:v0.9.6"
+version=$(get_version)
 
-echo "$IMAGE_TAG"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unknown tag: $(git describe)" >&2
+    exit 1
+fi
+
+IMAGE_TAG="michaelmell/moma:${version}"
 
 source ../env_vars.sh
 
+echo "Building image: ${IMAGE_TAG}"
 docker build .. --build-arg="GRB_VERSION=${GRB_VERSION}" --build-arg="GRB_SHORT_VERSION=${GRB_SHORT_VERSION}" -t "${IMAGE_TAG}"
-
-#docker tag 1b7127033a75 michaelmell/moma:v0.9.6
-#docker push michaelmell/moma:v0.9.6
-
-#docker run -it --mount type=bind,src="/home/micha/Documents/LicenseFiles/gurobi_web_license.lic",target="/opt/gurobi/gurobi.lic" moma:v0.9.3 /bin/bash

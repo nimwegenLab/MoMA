09a2d052eaccd8cbd0f141e838900bf993badba2
Michael Mell <michael.mell@unibas.ch>
2023-09-06 12:05:33 +0200

    Abort if desired version exists. And refactor.

--- tag_version.sh
+++ tag_version.sh
@@ -9,32 +9,38 @@ if [[ ! "$version_tag" =~ $version_regex ]]; then
   exit 1
 fi
 
+# generate list of version tags sorted by version number and print it on a single line:
+existing_versions_tags=$(git tag -l --sort=-v:refname | grep -E "^v[0-9]+\.[0-9]+\.[0-9]+$" | tr '\n' ' ')
+
+# Abort if the version tag alread exists in existing_versions_tags:
+if [[ "$existing_versions_tags" =~ (^|[[:space:]])"$version_tag"($|[[:space:]]) ]]; then
+  printf "ERROR: Version tag already exists: %s\n" "${version_tag}"
+  exit 1
+fi
+
 printf "\n"
 printf "Please answer the following questions before tagging the new version:\n"
 printf "\n"
 
-printf "Did you update CHANGELOG.md for the new version? (y/n)\n"
-read -r answer
-if [[ "$answer" != "y" ]]; then
-  printf "Aborting.\n"
-  exit 1
-fi
-printf "\n"
-
 printf "Here is a list of existing version tags:\n"
 
-# generate list of version tags sorted by version number and print it on a single line:
-version_tag_list=$(git tag -l --sort=-v:refname | grep -E "^v[0-9]+\.[0-9]+\.[0-9]+$" | tr '\n' ' ')
-
-printf "%s\n" "$version_tag_list"
+printf "%s\n" "$existing_versions_tags"
 printf "\n"
-printf "Is the version tag correct? (y/n)\n"
+printf "Do you want to continue with this version tag? (y/n)\n"
 printf "   %s\n" "$version_tag"
 read -r answer
 if [[ "$answer" != "y" ]]; then
   printf "Aborting.\n"
   exit 1
 fi
+
+printf "Did you update CHANGELOG.md for this new version (%s)? (y/n)\n" "${version_tag}"
+read -r answer
+if [[ "$answer" != "y" ]]; then
+  printf "Aborting.\n"
+  exit 1
+fi
+printf "\n"
 
 printf "\n"
 printf "Tagging version: %s\n" "$version_tag"
